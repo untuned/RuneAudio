b852650cf0446ff580df70e58be15bf2952408cb
rern <rernrern@gmail.com>
2017-02-22 14:11:55 +0700

    Create uninstall.sh

---
+++ aria2/uninstall.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+line2='\e[0;36m=========================================================\e[m'
+line='\e[0;36m---------------------------------------------------------\e[m'
+bar=$( echo -e "$(tput setab 6)   $(tput setab 0)" )
+info=$( echo $(tput setab 6; tput setaf 0) i $(tput setab 0; tput setaf 7) )
+
+# functions #######################################
+title2() {
+		echo -e "\n$line2\n"
+		echo -e "$bar $1"
+		echo -e "\n$line2\n"
+}
+title() {
+		echo -e "\n$line"
+		echo $1
+		echo -e "$line\n"
+}
+
+# check installed #######################################
+if ! pacman -Q aria2 > /dev/null 2>&1; then
+	title "$info aria2 not found."
+	exit
+fi
+
+title2 "$bar Uninstall aria2 ..."
+# uninstall package #######################################
+pacman -Rs --noconfirm aria2
+
+# remove files #######################################
+title "Remove files ..."
+rm -v /root/.config/aria2/aria2.conf
+rm -rfv /usr/share/nginx/html/aria2
+
+# restore modified files #######################################
+title "Restore modified files ..."
+echo '/etc/nginx/nginx.conf'
+sed -i '|server { #aria2|, |} #aria2| d' /etc/nginx/nginx.conf
+systemctl restart nginx
+
+# skip if reinstall - pwduninstall.sh re (any argument)
+[ $# -ne 0 ] && exit
+
+title2 "$bar aria2 successfully uninstalled."
+
+rm uninstall.sh
