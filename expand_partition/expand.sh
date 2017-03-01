05acacd80ce36a3fe02a3ee1b8fdb0310fda3813
rern <rernrern@gmail.com>
2017-03-01 13:58:37 +0700

    Update expand.sh

--- expand_partition/expand.sh
+++ expand_partition/expand.sh
@@ -9,6 +9,7 @@ linered='\e[0;31m---------------------------------------------------------\e[m'
 line2='\e[0;36m=========================================================\e[m'
 line='\e[0;36m---------------------------------------------------------\e[m'
 bar=$( echo -e "$(tput setab 6)   $(tput setab 0)" )
+warn=$( echo $(tput setab 1) ! $(tput setab 0) )
 info=$( echo $(tput setab 6; tput setaf 0) i $(tput setab 0; tput setaf 7) )
 
 # functions #######################################
@@ -40,14 +41,14 @@ else
 fi
 disk=${diskesc//\\/}
 
-freekb=$( df | sed -n 2p | awk '{print $4}' ) # free disk space in kB
+freekb=$( df | sed -n '/\/$/p' | awk '{print $4}' ) # free disk space in kB
 freemb=$( python -c "print($freekb / 1000)" ) # bash itself cannot do float
-unpartgb=$( sfdisk -F | sed -n '/'$diskesc'/,1p' | awk '{print $4}' ) # unpartitoned space in GB
-unpartkb=$( python -c "print($unpartgb * 1000000)" )
-sumkb=$(( $freekb + $unpartkb ))
+unpartb=$( sfdisk -F | sed -n '/'$diskesc'/p' | awk '{print $6}' ) # unpartitoned space in GB
+unpartmb=$( python -c "print($unpartb / 1000000)" )
+summb=$(( $freemb + $unpartmb ))
 
 # expand partition #######################################
-title "$info Available unused disk space: $unpartgb GB"
+title "$info Available unused disk space: $unpartmb MB"
 echo 'Current available free space' $freemb 'MB'
 echo
 echo 'Expand partiton to full unused space:'
@@ -72,15 +73,13 @@ case $answer in
 
 		resize2fs $devpart
 		if (( $? != 0 )); then
-			errorend "Failed: Expand partition\nTry - reboot > resize2fs $devpart"
+			errorend "$warn Failed: Expand partition\nTry - reboot > resize2fs $devpart"
 			exit
 		else
-			freekb=$( df | sed -n 2p | awk '{print $4}' )
-			freegb=$( python -c "print(round($freekb / 1000000, 2))" )
-
-			freegbr=$( LC_ALL=C /usr/bin/printf "%.*f\n" 2 $freegb ) # round to 2 decimal
+			freekb=$( df | sed -n '/\/$/p' | awk '{print $4}' )
+			freemb=$( python -c "print($freekb / 1000)" )
 			echo
-			echo $info 'Partiton now has' $freegbr 'GB free space.'
+			titleend "$info Partiton now has $freemb MB free space."
 		fi;;
 
 	* ) echo
