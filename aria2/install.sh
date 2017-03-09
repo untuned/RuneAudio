6c54aae1dbfd68c436c3504fed6132e087a2c4a2
rern <rernrern@gmail.com>
2017-03-09 17:59:11 +0700

    Update install.sh

--- aria2/install.sh
+++ aria2/install.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+line2='\e[0;36m=========================================================\e[m'
 line='\e[0;36m---------------------------------------------------------\e[m'
+bar=$( echo -e "$(tput setab 6)   $(tput setab 0)" )
+info=$( echo $(tput setab 6; tput setaf 0) i $(tput setab 0; tput setaf 7) )
+
+# functions #######################################
+title2() {
+		echo -e "\n$line2\n"
+		echo -e "$bar $1"
+		echo -e "\n$line2\n"
+}
 title() {
 		echo -e "\n$line"
 		echo $1
 		echo -e "$line\n"
-}
-titleend() {
-		echo -e "\n$1"
-		echo -e "\n$line\n"
 }
 
 rm install.sh
