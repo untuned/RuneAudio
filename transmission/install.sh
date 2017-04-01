47f79280fa486d4e15bff970a0b72156cc91664a
rern <rernrern@gmail.com>
2017-04-01 07:55:44 +0700

    Update install.sh

--- transmission/install.sh
+++ transmission/install.sh
@@ -92,7 +92,7 @@ esac
 
 title2 "Transmission installed successfully."
 echo 'Uninstall: ./uninstall_tran.sh'
-echo 'Download directory: /mnt/MPD/USB/hdd/transmission'
 echo 'Start: systemctl start transmission'
 echo 'Stop: systemctl stop transmission'
-titleend "Web Interface: [IP address]:9091"
+echo 'Download directory: /mnt/MPD/USB/hdd/transmission'
+titleend "WebUI: [RuneAudio_IP]:9091"
