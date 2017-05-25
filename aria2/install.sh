dcc3fc92e92fa2817e0878ca1eac945473a3f582
rern <rernrern@gmail.com>
2017-05-25 09:35:15 +0700

    Update install.sh

--- aria2/install.sh
+++ aria2/install.sh
@@ -35,12 +35,16 @@ if  grep '^Server = http://mirror.archlinuxarm.org/' /etc/pacman.d/mirrorlist; t
 	chmod +x rankmirrors.sh
 	./rankmirrors.sh
 fi
-if ! pacman -Q aria2 > /dev/null 2>&1; then
+if ! pacman -Q aria2 > &>/dev/null; then
 	title2 "Install Aria2 ..."
 	pacman -Sy --noconfirm aria2
 else
 	titleend "$info Aria2 already installed."
 	exit
+fi
+if ! pacman -Q glibc > &>/dev/null; then
+	title2 "Install GLIBC ..."
+	pacman -Sy --noconfirm glibc
 fi
 
 title "Get WebUI files ..."
