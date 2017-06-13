8037c05aaba67cd935d93c53b7b12fa26b721d99
rern <rernrern@gmail.com>
2017-06-13 15:04:51 +0700

    Create install.sh

--- transmission/install.sh
+++ transmission/install.sh
@@ -76,8 +76,8 @@ case $answer in
 		echo 'Password: '
 		read -s pwd
 		sed -i -e 's|"rpc-authentication-required": false|"rpc-authentication-required": true|
-		' -e 's|"rpc-password": ".*"|"rpc-password": "$pwd"|
-		' -e 's|"rpc-username": ".*"|"rpc-username": "root"|
+		' -e "s|\"rpc-password\": \".*\"|\"rpc-password\": \"$pwd\"|
+		" -e 's|"rpc-username": ".*"|"rpc-username": "root"|
 		' $file
 		;;
 	* ) echo;;
