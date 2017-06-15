3cb3cba22f0051320608b768506c9162b814bf9d
rern <rernrern@gmail.com>
2017-06-15 12:20:10 +0700

    Create expand.sh

--- expand_partition/expand.sh
+++ expand_partition/expand.sh
@@ -15,25 +15,27 @@ info=$( echo $(tput setab 6; tput setaf 0) i $(tput setab 0; tput setaf 7) )
 # functions #######################################
 
 title2() {
-		echo -e "\n$line2\n"
-		echo -e "$bar $1"
-		echo -e "\n$line2\n"
+	echo -e "\n$line2\n"
+	echo -e "$bar $1"
+	echo -e "\n$line2\n"
 }
 title() {
-		echo -e "\n$line"
-		echo $1
-		echo -e "$line\n"
+	echo -e "\n$line"
+	echo $1
+	echo -e "$line\n"
 }
 titleend() {
 		echo -e "\n$1"
 		echo -e "\n$line\n"
 }
 
-title "$info Remove all USB drives before proceeding:"
-echo 'To make sure only SD card to be expanded.'
-echo
-read -n 1 -s -p 'Press any key to continue ... '
-echo
+if ls /dev/sd* &>/dev/null; then
+	title "$info Remove all USB drives before proceeding:"
+	echo 'To make sure only SD card to be expanded.'
+	echo
+	read -n 1 -s -p 'Press any key to continue ... '
+	echo
+fi
 # partition data #######################################
 devpart=$( mount | grep 'on / type' | awk '{print $1}' )
 part=${devpart/\/dev\//}
