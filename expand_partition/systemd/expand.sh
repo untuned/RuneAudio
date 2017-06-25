51beb8b9843eb3b3e3247926047f456abc3e7064
rern <rernrern@gmail.com>
2017-06-25 09:22:23 +0700

    Update expand.sh

--- expand_partition/systemd/expand.sh
+++ expand_partition/systemd/expand.sh
@@ -2,9 +2,9 @@
 
 # 'partprope', 'libparted.so.2' needed
 
-unpartb=$( sfdisk -F | grep /dev/mmcblk0 | awk '{print $6}' )
+unpartmb=$( sfdisk -F | grep /dev/mmcblk0 | awk '{print $4}' )
 # noobs has 3MB unpartitioned space > skip
-if (($unpartb > 10)); then
+if (($unpartmb > 10)); then
 	echo -e 'd\n\nn\n\n\n\n\nw' | fdisk /dev/mmcblk0 &>/dev/null
 	partprobe /dev/mmcblk0
 	resize2fs /dev/mmcblk0p2
