fe57b64a504025e4a57fe7740a2b18aa9ab8ed0d
rern <rernrern@gmail.com>
2017-06-26 16:01:51 +0700

    Update install.sh

--- transmission/install.sh
+++ transmission/install.sh
@@ -53,7 +53,9 @@ sed -i -e 's|User=transmission|User=root|
 # refresh systemd services
 systemctl daemon-reload
 # create settings.json
-systemctl start transmission; systemctl stop transmission; file=$path/settings.json
+systemctl start transmission; systemctl stop transmission
+
+file=$path/settings.json
 sed -i -e 's|"download-dir": ".*"|"download-dir": "'"$path"'"|
 ' -e 's|"incomplete-dir": ".*"|"incomplete-dir": "'"$path"'/incomplete"|
 ' -e 's|"incomplete-dir-enabled": false|"incomplete-dir-enabled": true|
