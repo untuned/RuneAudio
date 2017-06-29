c6f603979ec620e711280af6268039dc2d3d6344
rern <rernrern@gmail.com>
2017-06-29 21:06:22 +0700

    Update install.sh

--- transmission/install.sh
+++ transmission/install.sh
@@ -37,11 +37,11 @@ rm transmission-cli-2.92-6-armv7h.pkg.tar.xz
 
 if mount | grep '/dev/sda1' &>/dev/null; then
 	mnt=$( mount | grep '/dev/sda1' | awk '{ print $3 }' )
-	mkdir -p $mnt/transmission
 	# interchange between os
 	label=${mnt##/*/}
 	mkdir -p /media
 	ln -s $mnt /media/$label
+	mkdir -p $mnt/transmission
 	path=/media/$label/transmission
 else
 	mkdir -p /root/transmission
