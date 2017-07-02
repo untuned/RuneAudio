ba09d2bf7e7449bf149f133d9c851a47c7fd1449
rern <rernrern@gmail.com>
2017-07-02 10:57:35 +0700

    Update install.sh

--- transmission/install.sh
+++ transmission/install.sh
@@ -35,36 +35,41 @@ else
 fi
 rm transmission-cli-2.92-6-armv7h.pkg.tar.xz
 
+# remove conf for non-exist user 'transmission'
+rm /usr/lib/tmpfiles.d/transmission.conf
+
 if mount | grep '/dev/sda1' &>/dev/null; then
 	mnt=$( mount | grep '/dev/sda1' | awk '{ print $3 }' )
-	# make 'media' a common path across os
-	label=${mnt##/*/}
-	mkdir -p /media
-	ln -s $mnt /media/$label
 	mkdir -p $mnt/transmission
-	path=/media/$label/transmission
+	path=$mnt/transmission
 else
 	mkdir -p /root/transmission
 	path=/root/transmission
 fi
 mkdir -p $path/{incomplete,watch}
 
-# user 'root'
-cp /lib/systemd/system/transmission.service /etc/systemd/system/transmission.service
-sed -i 's|User=.*|User=root|' /etc/systemd/system/transmission.service
-# refresh systemd services
+# custom systemd unit
+systemctl stop transmission
+systemctl disable transmission
+cp /lib/systemd/system/transmission*.service /etc/systemd/system/transmission.service
+sed -i 's|User=.*|User=root|
+' -e "|ExecStart| i\
+Environment=TRANSMISSION_HOME=$path\
+Environment=TRANSMISSION_WEB_HOME=$path/web
+" /etc/systemd/system/transmission.service
 systemctl daemon-reload
+
 # create settings.json
 systemctl start transmission; systemctl stop transmission
 
-file=/root/.config/transmission-daemon/settings.json
+file=$path/settings.json
 sed -i -e 's|"download-dir": ".*"|"download-dir": "'"$path"'"|
 ' -e 's|"incomplete-dir": ".*"|"incomplete-dir": "'"$path"'/incomplete"|
 ' -e 's|"incomplete-dir-enabled": false|"incomplete-dir-enabled": true|
 ' -e 's|"rpc-whitelist-enabled": true|"rpc-whitelist-enabled": false|
-' -e '/[^{},]$/ s/$/\,/
+' -e '/[^{},\{, \}]$/ s/$/, /
 ' -e '/}/ i\
-    "watch-dir": "'"$path"'/watch",\
+    "watch-dir": "'"$path"'/watch", \
     "watch-dir-enabled": true
 ' $file
 
@@ -88,9 +93,6 @@ esac
 # hash password by start
 systemctl start transmission
 
-# remove conf for user 'transmission'
-rm /usr/lib/tmpfiles.d/transmission.conf
-
 # web ui alternative
 title "$info Install WebUI alternative (Transmission Web Control):"
 echo -e '  \e[0;36m0\e[m No'
@@ -101,10 +103,11 @@ read -n 1 answer
 case $answer in
 	1 ) echo
 		wget -qN --show-progress https://github.com/ronggang/transmission-web-control/raw/master/release/transmission-control-full.tar.gz
-		mv /usr/share/transmission/web/index.html /usr/share/transmission/web/index.original.html
-		bsdtar -xf transmission-control-full.tar.gz -C /usr/share/transmission
+		mv /usr/share/transmission/web $path
+		mv $path/web/index.html $path/web/index.original.html
+		bsdtar -xf transmission-control-full.tar.gz -C $path
 		rm transmission-control-full.tar.gz
-		chown -R root:root /usr/share/transmission/web
+		chown -R root:root $path/web
 		;;
 	* ) echo;;
 esac
