a8ef9a28be4b38c16f0010c74cf5fef2cafbf42b
rern <rernrern@gmail.com>
2017-07-05 21:13:51 +0700

    Update install.sh

--- aria2/install.sh
+++ aria2/install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # import heading function
-wget -qN https://github.com/rern/tips/raw/master/bash/f_heading.sh
-. f_heading.sh
-rm f_heading.sh
+wget -qN https://github.com/rern/tips/raw/master/bash/f_heading.sh; . f_heading.sh; rm f_heading.sh
+
 rm install.sh
 
 if pacman -Q aria2 &>/dev/null; then
