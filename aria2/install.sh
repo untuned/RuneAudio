d77625ea204ea2fb3a394ec9727d06f447c861f3
rern <rernrern@gmail.com>
2017-07-08 18:17:09 +0700

    Update install.sh

--- aria2/install.sh
+++ aria2/install.sh
@@ -16,7 +16,7 @@ fi
 
 if (( $# == 0 )); then
 	# user input
-	title "$info Start Aria2 on system startup:"
+	titleinfo "Start Aria2 on system startup:"
 	echo -e '  \e[0;36m0\e[m No'
 	echo -e '  \e[0;36m1\e[m Yes'
 	echo
