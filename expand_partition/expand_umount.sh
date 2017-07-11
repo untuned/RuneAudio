2962cca0a62f5af58995876472161da63e9f557c
rern <rernrern@gmail.com>
2017-07-11 16:14:20 +0700

    Update expand_umount.sh

--- expand_partition/expand_umount.sh
+++ expand_partition/expand_umount.sh
@@ -4,12 +4,12 @@
 # https://github.com/rern/expand_partition
 
 # import heading function
-wget -qN https://github.com/rern/tips/raw/master/bash/f_heading.sh; . f_heading.sh; rm f_heading.sh
+wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 
 rm expand.sh
 
 if [[ ! -e /usr/bin/sfdisk ]] || [[ ! -e /usr/bin/python2 ]]; then
-	titleinfo "Unable to continue (sfdisk / python2 not found)."
+	title $info 'Unable to continue (sfdisk / python2 not found).'
 	exit
 fi
 
@@ -24,7 +24,7 @@ unpartmb=$( sfdisk -F | grep $disk | awk '{print $4}' )
 summb=$(( $freemb + $unpartmb ))
 # noobs has 3MB unpartitioned space
 if (($unpartmb < 10)); then
-	titleinfo "No useful space available. ( ${unpartmb}MB unused)"
+	title "$info No useful space available. ( ${unpartmb}MB unused)"
 	exit
 fi
 
@@ -32,14 +32,14 @@ fi
 if ls /dev/sd? &>/dev/null; then
 	hdd=$( ls /dev/sd? )
 	mnt=$( df | grep '/dev/sd' | awk '{print $NF}' )
-	titleinfo "Unmount and remove all USB drives before proceeding:"
+	title $info Unmount and remove all USB drives before proceeding:
 	echo 'Remove to make sure only SD card to be expanded.'
 	echo
 	echo -e "Drive: \e[0;36m$hdd\e[m"
 	
 	if df | grep '/dev/sd' &>/dev/null; then
 		echo -e "Mount: \e[0;36m$mnt\e[m"
-		titleinfo "Unmount: $mnt"
+		title $info Unmount: $mnt
 		echo -e '  \e[0;36m0\e[m No'
 		echo -e '  \e[0;36m1\e[m Yes'
 		echo
@@ -74,7 +74,7 @@ if ls /dev/sd? &>/dev/null; then
 fi
 
 # expand partition #######################################
-title2 "Expand partition"
+title -l = $bar Expand partition
 echo -e "Current partiton: \e[0;36m$devpart\e[m"
 echo -e "Current available free space: \e[0;36m$freemb MB\e[m"
 echo -e "Available unused disk space: \e[0;36m$unpartmb MB\e[m"
@@ -87,27 +87,27 @@ echo -e '\e[0;36m0\e[m / 1 ? '
 read -n 1 answer
 if [[ $answer == 1 ]]; then
 	if ! pacman -Q parted &>/dev/null; then
-		title "Get package file ..."
+		title Get package file ...
 		wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/expand_partition/parted-3.2-5-armv7h.pkg.tar.xz
 		pacman -U --noconfirm parted-3.2-5-armv7h.pkg.tar.xz
 		rm parted-3.2-5-armv7h.pkg.tar.xz
 	fi
-	title "Expand partiton ..."
+	title Expand partiton ...
 	echo -e 'd\n\nn\n\n\n\n\nw' | fdisk $disk &>/dev/null
 
 	partprobe $disk
 
 	resize2fs $devpart
 	if (( $? != 0 )); then
-		errorend "Failed: Expand partition\nTry - reboot > resize2fs $devpart"
+		title -c 1 "Failed: Expand partition\nTry - reboot > resize2fs $devpart"
 		exit
 	else
 		freekb=$( df | grep '/$' | awk '{print $4}' )
 		freemb=$( python2 -c "print($freekb / 1000)" )
 		echo
-		titleinfo "Partiton \e[0;36m$devpart\e[m now has \e[0;36m$freemb\e[m MB free space."
+		title "$info Partiton \e[0;36m$devpart\e[m now has \e[0;36m$freemb\e[m MB free space."
 	fi
 else
-	titleinfo "Expand partition cancelled."
+	title $info Expand partition cancelled.
 	exit
 fi
