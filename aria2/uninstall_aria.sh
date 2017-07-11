bad4c89774b271280b93de90f24088c69ec91551
rern <rernrern@gmail.com>
2017-07-11 16:07:57 +0700

    Update uninstall_aria.sh

--- aria2/uninstall_aria.sh
+++ aria2/uninstall_aria.sh
@@ -5,11 +5,11 @@ wget -qN https://github.com/rern/tips/raw/master/bash/f_heading.sh; . f_heading.
 
 # check installed #######################################
 if ! pacman -Q aria2 &>/dev/null; then
-	titleinfo "Aria2 not found."
+	title $info Aria2 not found.
 	exit
 fi
 
-title2 "Uninstall Aria2 ..."
+title -l = $bar Uninstall Aria2 ...
 systemctl disable aria2
 systemctl stop aria2
 rm /etc/systemd/system/aria2.service
@@ -18,7 +18,7 @@ systemctl daemon-reload
 pacman -Rs --noconfirm aria2
 
 # remove files #######################################
-title "Remove files ..."
+title Remove files ...
 if mount | grep '/dev/sda1'; then
 	mnt=$( mount | grep '/dev/sda1' | awk '{ print $3 }' )
 	rm -rv $mnt/aria2/web
@@ -27,6 +27,6 @@ else
 fi
 rm -rv /root/.config/aria2
 
-titleend "Aria2 uninstalled successfully."
+title -nt Aria2 uninstalled successfully.
 
 rm uninstall_aria.sh
