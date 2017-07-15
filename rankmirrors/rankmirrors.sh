ffbb732149f5fa1be51adff4d07fa8166fc6fb77
rern <rernrern@gmail.com>
2017-07-15 19:47:55 +0700

    Update rankmirrors.sh

--- rankmirrors/rankmirrors.sh
+++ rankmirrors/rankmirrors.sh
@@ -64,12 +64,12 @@ rank=$( echo -e "$dl_server" | sort -k1 -n -r ) # sort by '1st column' 'as numbe
 rankfile=$( echo -e "$rank" | cut -f2-4 -d' ' ) # keep '2nd-4th column' 'devided by space' (remove 1st column)
 
 echo
-echo -e '\e[0;36m/etc/pacman.d/mirrorlist\e[m was updated with these servers top the list:'
+echo -e "\e[0;36m/etc/pacman.d/mirrorlist\e[m was updated with these servers top the list:"
 echo
 echo -e "$rankfile" | sed -n 1,3p
 echo
-title -l = $bar Mirror list ranked successfully.
-title -nt Update package database: pacman -Sy
+title -l = "$bar Mirror list ranked successfully."
+title -nt "Update package database: pacman -Sy"
 
 [ ! -f $list'.original' ] && cp $list $list'.original' # skip if already backup
 echo -e "$rankfile" > $list
