459bfbc6b4d2599b446fda827cc55fcd73bb79b0
rern <rernrern@gmail.com>
2017-07-15 19:53:40 +0700

    Update uninstall_tran.sh

--- transmission/uninstall_tran.sh
+++ transmission/uninstall_tran.sh
@@ -5,20 +5,20 @@ wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; r
 
 # check installed #######################################
 if ! pacman -Q transmission-cli &>/dev/null; then
-	title $info Transmission not found.
+	title "$info Transmission not found."
 	exit
 fi
 
-title2 "Uninstall Transmission ..."
+title -l = "Uninstall Transmission ..."
 # uninstall package #######################################
 pacman -R --noconfirm transmission-cli
 
 # remove files #######################################
-title Remove files ...
+title "Remove files ..."
 systemctl disable transmission
 rm /etc/systemd/system/transmission.service
 systemctl daemon-reload
 
-title -l = $bar Transmission uninstalled successfully.
+title -l = "$bar Transmission uninstalled successfully."
 
 rm uninstall_tran.sh
