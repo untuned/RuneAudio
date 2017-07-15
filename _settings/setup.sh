2a202b5b9439f5a1bd0134b987e82af5a3e330f0
rern <rernrern@gmail.com>
2017-07-15 22:46:19 +0700

    Update setup.sh

--- _settings/setup.sh
+++ _settings/setup.sh
@@ -92,27 +92,33 @@ rm /tmp/p7/vendor # noobs marker for update prompt
 title "$bar Set settings ..."
 #################################################################################
 {
-  redis-cli set usb_db_autorebuild 0     # usb auto rebuild
-
-  #redis-cli set hostname runeaudio      # hostname
-  #hostnamectl set-hostname runeaudio
-  #redis-cli set ntpserever pool.ntp.org # ntpserever
-  redis-cli set timezone Asia/Bangkok    # timezone
+	### Sources ###
+	redis-cli set usb_db_autorebuild 0     # usb auto rebuild
+	
+	### MPD ###
+	#redis-cli set ao bcm2835 ALSA_1       # audio output (string*)
+	#redis-cli set volume 0                # volume control
+	#redis-cli set mpd_start_volume -1     # start volume
+	#redis-cli set dynVolumeKnob 0         # volume knob
+	
+	### Settings ###
+	#redis-cli set hostname runeaudio      # hostname (string)
+	#redis-cli set ntpserever pool.ntp.org # ntpserever (string)
+	redis-cli set timezone Asis/Bangkok    # timezone (string from list)
   timedatectl set-timezone Asia/Bangkok
-  #redis-cli set orionprofile RuneAudio  # sound signature 
-  redis-cli hset airplay enable 0        # airplay
-  #hset spotify enable 0                 # spotify
-  redis-cli hset dlna enable 0           # upnp/dlna
-  #redis-cli set local_browser 0         # local browser
-  redis-cli set udevil 0                 # usb automount
-  #redis-cli set coverart 0              # usb automount
-  #redis-cli hset lastfm enable 0        # upnp/dlna
-
-  #redis-cli set opcache 0               # opcache
-  #redis-cli set playerid id             # playerid
-  #redis-cli hset get branch dev         # git branch
-  #redis-cli set dev 0                   # dev mode
-  #redis-cli set debug 0                 # debug
+	#redis-cli set orionprofile RuneAudio  # sound signature (string from list)
+	redis-cli hset airplay enable 0        # airplay
+	#hset spotify enable 0                 # spotify
+	redis-cli hset dlna enable 0           # upnp/dlna
+	#redis-cli set local_browser 0         # local browser
+	redis-cli set udevil 0                 # usb automount
+	#redis-cli set coverart 0              # album cover
+	#redis-cli hset lastfm enable 0        # upnp/dlna
+	
+	### Development ###
+	#redis-cli set opcache 0               # opcache
+	#redis-cli set dev 1                   # dev mode
+	#redis-cli set debug 0                 # debug
 } &> /dev/null
 
 # reboot command and motd
