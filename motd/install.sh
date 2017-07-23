ebb19e7b8f057a778c6f25d04773487826060b04
rern <rernrern@gmail.com>
2017-07-23 20:28:39 +0700

    Update install.sh

--- motd/install.sh
+++ motd/install.sh
@@ -4,7 +4,7 @@ wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; r
 
 title -l = "$bar Install Rune logo motd ..."
 wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/motd/uninstall_motd.sh; chmod +x uninstall_motd.sh
-wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/_settings/motd.banner -P /etc
+wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/motd/motd.banner -P /etc
 echo '#!/bin/bash
 echo -e "$( < /etc/motd.banner )"
 ' > /etc/profile.d/motd.sh
