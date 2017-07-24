a049f6d3ab7b4b0b872c993f0cefaf0804a2a6f6
rern <rernrern@gmail.com>
2017-07-24 20:48:10 +0700

    Update cmd.sh

--- _settings/cmd.sh
+++ _settings/cmd.sh
@@ -40,34 +40,57 @@ bootrune() {
 	reboot
 }
 
-yesno() {
-	echo
-	echo "$1"
-	echo -e '  \e[0;36m0\e[m No'
-	echo -e '  \e[0;36m1\e[m Yes'
-	echo
-	echo -e '\e[0;36m0\e[m / 1 ? '
-	read -n 1 ans
-}
-hardresetosmc() {
-	yesno 'Reset to virgin OSMC?'	
-	if [[ $ans == 1 ]]; then
-		mountmmc 1
-		umount -l /dev/mmcblk0p7
-		mkfs.ext4 /dev/mmcblk0p7
-		mountmmc 7
-		bsdtar -xvf /tmp/p1/os/OSMC/root-rbp2.tar.xz -C /tmp/p7
-	fi
+resetosmc() {
+	umount -l /dev/mmcblk0p7 &> /dev/null
+	echo y | mkfs.ext4 /dev/mmcblk0p7 &> /dev/null
+	mountmmc 7
+	mountmmc 1
+	bsdtar -xvf /tmp/p1/os/OSMC/root-rbp2.tar.xz -C /tmp/p7
+	
+	# fstab
+	mnt0=$( mount | grep '/dev/sda1' | awk '{ print $3 }' )
+	label=${mnt0##/*/}
+	mnt="/mnt/$label"
+	echo "$vfat_part  /boot    vfat     defaults,noatime,noauto,x-systemd.automount    0   0
+	$ext4_part      /                  ext4     defaults,noatime  0   0
+	/dev/mmcblk0p1  /media/RECOVERY    vfat     noauto,noatime    0   0
+	/dev/mmcblk0p5  /media/SETTINGS    ext4     noauto,noatime    0   0
+	/dev/mmcblk0p8  /media/boot        vfat     noauto,noatime    0   0
+	/dev/mmcblk0p9  /media/root        ext4     noauto,noatime    0   0
+	/dev/sda1       $mnt               ext4     defaults,noatime  0   0
+	" > /tmp/p7/etc/fstab
+	# customize files
+	sed -i "s/root:.*/root:\$6\$X6cgc9tb\$wTTiWttk\/tRwPrM8pLZCZpYpHE8zEar2mkSSQ7brQvflqhA5K1dgcyU8nzX\/.tAImkMbRMR0ex51LjPsIk8gm0:17000:0:99999:7:::/" /tmp/p7/etc/shadow
+	sed -i "s/PermitRootLogin without-password/PermitRootLogin yes/" /tmp/p7/etc/ssh/sshd_config
+	cp -r /tmp/p1/os/OSMC/custom/. /tmp/p7
+	chmod 644 /tmp/p7/etc/udev/rules.d/usbsound.rules
+	chmod 755 /tmp/p7/home/osmc/*.py
+	chown -R 1000:1000 /tmp/p7/home/osmc
+
+	# packages cache
+	mkdir -p $mnt/varcache/apt
+	rm -r /tmp/p7/var/cache/apt
+	ln -s $mnt/varcache/apt /tmp/p7/var/cache/apt
+	# disable setup marker files
+	touch /tmp/p7/walkthrough_completed # initial setup
+	rm /tmp/p7/vendor # noobs marker for update prompt
 }
 
-}
 hardreset() {
-	yesno 'Reset to virgin NOOBS?'
-	if [[ $ans == 1 ]]; then
-		mkdir /tmp/p1
-		mount /dev/mmcblk0p1 /tmp/p1
-		echo -n " forcetrigger" >> /tmp/p1/recovery.cmdline
-		/var/www/command/rune_shutdown
-		reboot
-	fi
+	echo
+	echo "Reset to virgin OS:"
+	echo -e '  \e[0;36m0\e[m Cancel'
+	echo -e '  \e[0;36m1\e[m OSMC'
+	echo -e '  \e[0;36m2\e[m NOOBS: OSMC + Rune'
+	echo
+	echo -e '\e[0;36m0\e[m / 1 / 2 ? '
+	read ans
+	case $ans in
+		1) resetosmc;;
+		2) mountmmc 1
+			echo -n " forcetrigger" >> /tmp/p1/recovery.cmdline
+			/var/www/command/rune_shutdown
+			reboot;;
+		*) ;;
+	esac
 }
