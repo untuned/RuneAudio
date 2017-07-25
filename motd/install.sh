eab2c500b91d0006f776f33896fa757031dc47f0
rern <rernrern@gmail.com>
2017-07-25 16:49:51 +0700

    Update install.sh

--- motd/install.sh
+++ motd/install.sh
@@ -4,19 +4,17 @@ rm $0
 
 wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 
+mv /etc/motd{,.original}
+
 title -l = "$bar Install Rune logo motd ..."
 
 wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/motd/uninstall_motd.sh; chmod +x uninstall_motd.sh
-wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/motd/motd.banner -P /etc
+wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/motd/motd -P /etc
 
 echo '#!/bin/bash
 color=51
-echo -e "\e[38;5;${color}m$( < /etc/motd.banner )\e[0m\n"
-#PS1='\''\u@\e[38;5;${color}m\h\e[0m:\W\$ '\'' # single quote only
-#' > /etc/profile.d/motd.sh
-#sed -i '/PS1=/ s/^/#/' /etc/bash.bashrc
-
-mv /etc/motd{,.original}
+echo -e "\e[38;5;${color}m$( < /etc/motd )\e[0m\n"
+' > /etc/profile.d/motd.sh
 
 echo -e "\nUninstall: ./uninstall_motd.sh"
 title -nt "$info Relogin to see new Rune logo motd."
