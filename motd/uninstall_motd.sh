fab9ee6e4811542579fba322bb297a996c842996
rern <rernrern@gmail.com>
2017-07-25 17:02:06 +0700

    Update uninstall_motd.sh

--- motd/uninstall_motd.sh
+++ motd/uninstall_motd.sh
@@ -5,11 +5,11 @@ wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; r
 title -l = "$bar Uninstall Rune logo motd ..."
 
 mv /etc/motd{.original,}
-rm /etc/motd.banner
+rm /etc/motd.logo
 rm /etc/profile.d/motd.sh
 
 sed -i '/PS1/ s/^#//' /etc/bash.bashrc
 
-title -nt "$info Relogin to see original motd."
+title -nt "\n$info Relogin to see original motd."
 
 rm $0
