20b43c6ff8326bfb30a099ce70377ad32ae8b12a
rern <rernrern@gmail.com>
2017-07-29 20:58:43 +0700

    Update cmd.sh

--- _settings/cmd.sh
+++ _settings/cmd.sh
@@ -78,7 +78,7 @@ $vfat_part      /boot            vfat  defaults,noatime  0    0
 	echo "$fstabcontent" | column -t > $file
 	w=$( wc -L < $file )                 # widest line
 	hr=$( printf "%${w}s\n" | tr ' ' - ) # horizontal line
-	sed -i '1 a'$hr $file
+	sed -i '1 a\#'$hr $file
 
 	# customize files
 	sed -i "s/root:.*/root:\$6\$X6cgc9tb\$wTTiWttk\/tRwPrM8pLZCZpYpHE8zEar2mkSSQ7brQvflqhA5K1dgcyU8nzX\/.tAImkMbRMR0ex51LjPsIk8gm0:17000:0:99999:7:::/" /tmp/p7/etc/shadow
