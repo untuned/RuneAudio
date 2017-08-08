3638bb4d97301a1c031e2da78c631e2615a8250c
rern <rernrern@gmail.com>
2017-08-08 15:52:09 +0700

    Update install.sh

--- motd/install.sh
+++ motd/install.sh
@@ -2,7 +2,7 @@
 
 rm $0
 
-[[ -e /etc/motd.logo ]] && echo -e '\n"Rune logo motd" already installed\n'; exit
+[[ -e /etc/motd.logo ]] && echo -e '\n"Rune logo motd" already installed.\n'; exit
 
 wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 
