57686de41a2a03573f6ed95bc421a48c5add12d2
rern <rernrern@gmail.com>
2017-08-10 18:22:31 +0700

    Update install.sh

--- transmission/install.sh
+++ transmission/install.sh
@@ -8,7 +8,7 @@
 rm $0
 
 # import heading function
-wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
+wget -qN https://raw.githubusercontent.com/rern/title_script/master/title.sh; . title.sh; rm title.sh
 timestart
 
 if pacman -Q transmission-cli &>/dev/null; then
@@ -31,9 +31,10 @@ else # with arguments
 	(( $# > 2 )) && ansstartup=$3 || ansstartup=0
 fi
 
-wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/transmission/uninstall_tran.sh
+gitpath=https://raw.githubusercontent.com/rern/RuneAudio/master/transmission
+wget -qN --show-progress $gitpath/uninstall_tran.sh
 chmod +x uninstall_tran.sh
-wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/transmission/_repo/transmission/transmission-cli-2.92-6-armv7h.pkg.tar.xz
+wget -qN --show-progress $gitpath/_repo/transmission/transmission-cli-2.92-6-armv7h.pkg.tar.xz
 
 title -l = "$bar Install Transmission ..."
 pacman -U --noconfirm transmission-cli-2.92-6-armv7h.pkg.tar.xz
