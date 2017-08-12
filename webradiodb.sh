ba89b6e276e380797a0895fda980cf6240da57ac
rern <rernrern@gmail.com>
2017-08-12 19:24:54 +0700

    Update webradiodb.sh

--- webradiodb.sh
+++ webradiodb.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-# populate webradio files
-
-# cp *.pls /mnt/MPD/Webradio
-# ./webradiodb.sh
-
 echo
 
+# fix sorting
+file=/srv/http/assets/js/runeui.js
+if ! grep 'a.playlist > b.playlist' $file; then
+	line=$(( $( sed -n '/id="webradio-add"/=' $file ) - 9 ))
+	sed -i $line' i\
+            data.sort(function(a, b){\
+                if (a.playlist < b.playlist)\
+                    return -1;\
+                if (a.playlist > b.playlist)\
+                    return 1;\
+                return 0;\
+            });
+	' $file
+fi
+
+# clear database
 redis-cli del webradios
 
+# add data from files
 for file in /mnt/MPD/Webradio/*.pls; do
 	name=$( grep 'Title1' "$file" | cut -d '=' -f2 )
 	url=$( grep 'File1' "$file" | cut -d '=' -f2 )
@@ -17,6 +29,7 @@ for file in /mnt/MPD/Webradio/*.pls; do
 	printf "%-30s : $url\n" "$name"
 done
 
+# refresh ui
 mpc update Webradio &> /dev/null
 
 echo -e '\nWebradio imported successfully.\n'
