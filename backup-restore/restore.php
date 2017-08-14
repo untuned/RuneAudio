6639fe4070a710daa35cf2f6719d62dcf983a4f9
rern <rernrern@gmail.com>
2017-08-14 18:59:53 +0700

    Update restore.php

--- backup-restore/restore.php
+++ backup-restore/restore.php
@@ -5,12 +5,6 @@ $filetmp = $file['tmp_name'];
 $filedest = '/srv/http/'.$filename;
 $filesize = filesize($filetmp);
 
-echo '<br>name = '.$filename;
-echo '<br>tmp_name = '.$filetmp;
-echo '<br>dest = '.$filedest;
-echo '<br>size = '.$filesize;
-echo '<br>';
-
 if ($filesize === 0) die('File upload error !');
 if (! move_uploaded_file($filetmp, $filedest)) die('File move error !');
 
