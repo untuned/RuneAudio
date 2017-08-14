7405554b178fb71f2be71b683910badf0b17dd6b
rern <rernrern@gmail.com>
2017-08-14 20:59:44 +0700

    Update modify.sh

--- backup-restore/modify.sh
+++ backup-restore/modify.sh
@@ -21,13 +21,22 @@ function wrk_backup(\$bktype) {\
 }
 ' -e '/function wrk_restore/, /^}/ s|^|//|
 ' -e '/function wrk_restore/ i\
-function wrk_restore(\$backupfile) {\
-    \$file = "/tmp/backup\.tar\.xz";\
-    \$cmdstring = "systemctl stop mpd redis; ";\
-    \$cmdstring \.= "bsdtar -xf "\.\$path\." -C /; ";\
-    \$cmdstring \.= "systemctl start mpd redis; ";\
-    \$cmdstring \.= "mpc update Webradio";\
-    \$cmdstring \.= "rm \$file";\
+function wrk_backup(\$bktype) {\
+	\$file = "/run/backup_"\.date('Ymd-hi')\."\.tar\.xz";\
+	\$dir = '--exclude /etc/netctl/examples'\.\
+	    ' /var/lib/redis/rune\.rdb'\.\
+	    ' /var/lib/mpd'\.\
+		' /etc/mpd\.conf'\.\
+		' /etc/mpdscribble\.conf'\.\
+		' /etc/spop'\.\
+		' /etc/localtime'\.\
+		' /etc/netctl'\.\
+		' /mnt/MPD/Webradio'\.\
+	;\
+	\$exclude = '--exclude /etc/netctl/examples';\
+	\$cmdstring = "redis-cli save; ";\
+	\$cmdstring \.= "bsdtar -czf \$file \$dir";\
     sysCmd(\$cmdstring);\
+    return \$file;\
 }
 ' /srv/http/app/libs/runeaudio.php
