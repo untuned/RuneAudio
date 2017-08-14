5f7327c5feb8acb03daa825a16918e4ce85c02dd
rern <rernrern@gmail.com>
2017-08-14 18:27:33 +0700

    Create upload.php

---
+++ backup-restore/upload.php
@@ -0,0 +1,19 @@
+<?php
+$file = $_FILES['filebackup'];
+$filename = $file['name'];
+$filetmp = $file['tmp_name'];
+$filedest = '/srv/http/'.$filename;
+$filesize = filesize($filetmp);
+
+echo '<br>name = '.$filename;
+echo '<br>tmp_name = '.$filetmp;
+echo '<br>dest = '.$filedest;
+echo '<br>size = '.$filesize;
+echo '<br>';
+
+if ($filesize === 0) die('File upload error !');
+if (! move_uploaded_file($filetmp, $filedest)) die('File move error !');
+
+$restore = exec("sudo /srv/http/extract.sh $filedest; echo $?");
+
+if ($restore == 1) die('Restore failed !');
