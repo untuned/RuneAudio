29653a5f3f51a0cdc06fea44353524e8c557c25c
rern <rernrern@gmail.com>
2017-08-15 16:53:35 +0700

    Update install.sh

--- backup-restore/install.sh
+++ backup-restore/install.sh
@@ -29,3 +29,56 @@ sed -i -e '/value="backup"/ {n;n;n;n;n;n; s/method="post"/id="restore"/}
 ' -e 's/type="file"/& name="filebackup"/
 ' -e'/value="restore"/ s/name="syscmd" value="restore" //; s/type="submit" disabled>Upload/disabled>Restore/
 ' /srv/http/app/templates/settings.php
+
+jsrestore="
+$('#restore').submit(function() {
+	var formData = new FormData($(this)[0]);
+	$.ajax({
+		url: '../../restore.php',
+		type: 'POST',
+		data: formData,
+		cache: false,
+		contentType: false,
+		enctype: 'multipart/form-data',
+		processData: false,
+		success: function (response) {
+			alert(response);
+		}
+	});
+	return false
+});
+" 
+echo "$jsrestore" >> /srv/http/assets/js/runeui.js
+echo "$jsrestore" >> /srv/http/assets/js/runeui.min.js
+
+echo '<?php
+$file = $_FILES["filebackup"];
+$filename = $file["name"];
+$filetmp = $file["tmp_name"];
+$filedest = "/srv/http/tmp/$filename";
+$filesize = filesize($filetmp);
+
+if ($filesize === 0) die("File upload error !");
+if (! move_uploaded_file($filetmp, $filedest)) die("File move error !");
+
+$restore = exec("sudo /srv/http/restore.sh $filedest; echo $?");
+
+if ($restore == 0) {
+	echo "Restored successfully.";
+} else {
+	echo "Restore failed !";
+}
+' > /srv/http/restore.php
+
+echo '#!/bin/bash
+
+systemctl stop mpd redis
+bsdtar -xpf $1 -C /
+systemctl start mpd redis
+mpc update Webradio
+hostnamectl set-hostname $( redis-cli get hostname )
+rm $1
+' > /srv/http/restore.sh
+
+echo 'http ALL=NOPASSWD: ALL' > /etc/sudoers.d/sudoers
+chmod 755 -R /etc/sudoers.d/
