26e2c99c405a0c4b4e3f9f55bee28f705a06bfb9
rern <rernrern@gmail.com>
2017-08-17 10:40:54 +0700

    Update uninstall_back.sh

--- backup-restore/uninstall_back.sh
+++ backup-restore/uninstall_back.sh
@@ -11,10 +11,19 @@ if [[ ! -e /srv/http/restore.php ]]; then
 fi
 
 rm -r /srv/http/tmp
-mv -f /srv/http/app/libs/runeaudio.php{.original,}
-mv -f /srv/http/app/templates/settings.php{.original,}
-mv -f /srv/http/assets/js/runeui.js{.original,}
-mv -f /srv/http/assets/js/runeui.min.js{.original,}
+sed -i -e '\|/run/backup_|,+1 s|^//||
+' -e '\|/srv/http/tmp|,/^ \+;/ d
+' /srv/http/app/libs/runeaudio.php
+
+sed -i -e 's/id="restore"/method="post"/
+' -e 's/type="file" name="filebackup"/type="file"/
+' -e 's/id="btn-backup-upload"/ s/id="btn-backup-upload"/& name="syscmd" value="restore"/; s/disabled>Restore/type="submit" disabled>Upload/
+'/srv/http/app/templates/settings.php
+
+sed -i '/#restore/,/});/ d' /srv/http/assets/js/runeui.js
+
+sed -i '/#restore/ d' /srv/http/assets/js/runeui.min.js
+
 rm /srv/http/restore.* /etc/sudoers.d/sudoers
 
 systemctl restart rune_SY_wrk
