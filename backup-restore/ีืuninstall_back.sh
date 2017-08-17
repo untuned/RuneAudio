8cf562e1a7b8f7fa561f37a41b1edf6e32ecf948
rern <rernrern@gmail.com>
2017-08-17 10:06:12 +0700

    Create ีืuninstall_back.sh

---
+++ backup-restore/ีืuninstall_back.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# import heading function
+wget -qN https://raw.githubusercontent.com/rern/title_script/master/title.sh; . title.sh; rm title.sh
+
+title -l = "$bar Uninstall Backup-Restore update ..."
+
+if [[ ! -e /srv/http/restore.php ]]; then
+    echo -e "$info Uninstall Backup-Restore update not found."
+    exit
+fi
+
+rm -r /srv/http/tmp
+mv -f /srv/http/app/libs/runeaudio.php{.original,}
+mv -f /srv/http/app/templates/settings.php{.original,}
+mv -f /srv/http/assets/js/runeui.js{.original,}
+mv -f /srv/http/assets/js/runeui.min.js{.original,}
+rm /srv/http/restore.* /etc/sudoers.d/sudoers
+
+systemctl restart rune_SY_wrk &
+
+if pgrep midori >/dev/null; then
+	killall midori
+	sleep 1
+	xinit &>/dev/null &
+	echo 'Local browser restarted.'
+fi
+
+title -l = "$bar Backup-Restore update uninstalled successfully."
+
+rm #0
