761d4d416b22ff40f9c62548adb547d1c2a04698
rern <rernrern@gmail.com>
2017-08-17 10:52:10 +0700

    Update uninstall_back.sh

--- backup-restore/uninstall_back.sh
+++ backup-restore/uninstall_back.sh
@@ -20,9 +20,9 @@ sed -i -e 's/id="restore"/method="post"/
 ' -e 's/id="btn-backup-upload"/ s/id="btn-backup-upload"/& name="syscmd" value="restore"/; s/disabled>Restore/type="submit" disabled>Upload/
 '/srv/http/app/templates/settings.php
 
-sed -i '/#restore/,/});/ d' /srv/http/assets/js/runeui.js
+sed -i '/#restore/,/^});/ d' /srv/http/assets/js/runeui.js
 
-sed -i '/#restore/ d' /srv/http/assets/js/runeui.min.js
+sed -i 's/\$("#restore").\+});//' /srv/http/assets/js/runeui.min.js
 
 rm /srv/http/restore.* /etc/sudoers.d/sudoers
 
