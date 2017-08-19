fd01ec1f3ebd6b17437e89cb56bfa5407c8a6bd9
rern <rernrern@gmail.com>
2017-08-19 09:51:07 +0700

    Create addons.php

---
+++ Addons/addons.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>RuneAudio - Addons</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="msapplication-tap-highlight" content="no" />
+    <link rel="stylesheet" href="assets/css/runeui.css">
+    <link rel="stylesheet" href="assets/css/gpiosettings.css">
+    <link rel="shortcut icon" href="assets/img/favicon.ico">
+<style>
+	#addons .boxed-group {
+		padding: 20px;
+	}
+	#addons .btn {
+		text-transform: capitalize;
+	}
+</style>
+</head>
+<body>
+
+<div id="addons" class="container">
+<h1>ADDONS</h1><a id="close"><i class="fa fa-times fa-lg"></i></a>
+
+<div class="boxed-group">
+<legend>Aria2</legend>
+<form class="form-horizontal">
+<p>Download utility that supports HTTP(S), FTP, BitTorrent, and Metalink. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/aria2/README.md">GitHub</a> )</p>
+<a id="inaria" class="btn btn-primary">Install</a> &nbsp; <a id="unaria" class="btn btn-default">Uninstall</a>
+</form>
+</div>
+
+<div class="boxed-group">
+<legend>Backup-Restore Update</legend>
+<form class="form-horizontal">
+<p>Enable backup-restore settings and databases. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/backup-restore/README.md">GitHub</a> )</p>
+<a id="inback" class="btn btn-primary">Install</a> &nbsp; <a id="unback" class="btn btn-default">Uninstall</a>
+</form>
+</div>
+
+<div class="boxed-group">
+<legend>Expand Partition</legend>
+<form class="form-horizontal">
+<p>Expand default 2GB partition to full capacity of SD card. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/expand_partition/README.md">GitHub</a> )</p>
+<a id="inexpa" class="btn btn-primary">Install</a> &nbsp; <a id="unexpa" class="btn btn-default">Uninstall</a>
+</form>
+</div>
+
+<div class="boxed-group">
+<legend>Rank Mirror Packages Servers</legend>
+<form class="form-horizontal">
+<p>Fix packages download errors. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/rankmirrors/README.md">GitHub</a> )</p>
+<a id="inrank" class="btn btn-primary">Install</a> &nbsp; <a id="unrank" class="btn btn-default">Uninstall</a>
+</form>
+</div>
+
+<div class="boxed-group">
+<legend>RuneAudio Logo for SSH Terminal</legend>
+<form class="form-horizontal">
+<p>Message of the day - RuneAudio Logo and dimmed command prompt. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/motd/README.md">GitHub</a> )</p>
+<a id="inmotd" class="btn btn-primary">Install</a> &nbsp; <a id="unmotd" class="btn btn-default">Uninstall</a>
+</form>
+</div>
+
+<div class="boxed-group">
+<legend>RuneUI Enhancements</legend>
+<form class="form-horizontal">
+<p>More minimalism and more fluid layout. ( More detail on <a href="https://github.com/rern/RuneUI_enhancement/blob/master/README.md">GitHub</a> )</p>
+<a id="inenha" class="btn btn-primary">Install</a> &nbsp; <a id="unenha" class="btn btn-default">Uninstall</a>
+</form>
+</div>
+
+<div class="boxed-group">
+<legend>RuneUI GPIO</legend>
+<form class="form-horizontal">
+<p>GPIO connected relay module control. ( More detail on <a href="https://github.com/rern/RuneUI_GPIO/blob/master/README.md">GitHub</a> )</p>
+<a id="ingpio" class="btn btn-primary">Install</a> &nbsp; <a id="ungpio" class="btn btn-default">Uninstall</a>
+</form>
+</div>
+
+<div class="boxed-group">
+<legend>Transmission</legend>
+<form class="form-horizontal">
+<p>Fast, easy, and free BitTorrent client. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/transmission/README.md">GitHub</a> )</p>
+<a id="intran" class="btn btn-primary">Install</a> &nbsp; <a id="untran" class="btn btn-default">Uninstall</a>
+</form>
+</div>
+
+<div class="boxed-group">
+<legend>Webradio import</legend>
+<form class="form-horizontal">
+<p>Webradio files import script. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/webradio/README.md">GitHub</a> )</p>
+<a id="inwebr" class="btn btn-primary">Install</a> &nbsp; <a id="unwebr" class="btn btn-default">Uninstall</a>
+</form>
+</div>
+
+</div>
+<script src="assets/js/vendor/jquery-2.1.0.min.js"></script>
+<script src="assets/js/vendor/bootstrap.min.js"></script>
+<script src="assets/js/vendor/bootstrap-select-1.12.1.min.js"></script>
+
+</body>
+</html>
