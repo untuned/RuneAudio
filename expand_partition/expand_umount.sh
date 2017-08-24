9b85b4d47ef5ed9d1fe3e979605da48476af3200
rern <rernrern@gmail.com>
2017-08-24 18:05:53 +0700

    Update expand_umount.sh

--- expand_partition/expand_umount.sh
+++ expand_partition/expand_umount.sh
@@ -6,7 +6,7 @@
 rm $0
 
 # import heading function
-wget -qN --no-check-certificate https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
+wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 
 if [[ ! -e /usr/bin/sfdisk ]] || [[ ! -e /usr/bin/python2 ]]; then
 	echo -e "$info Unable to continue (sfdisk / python2 not found)."
