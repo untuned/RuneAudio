a5e22a64d6b05b794a466b8e04597c0c2966b87b
rern <rernrern@gmail.com>
2017-08-24 18:04:09 +0700

    Update uninstall_aria.sh

--- aria2/uninstall_aria.sh
+++ aria2/uninstall_aria.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # import heading function
-wget -qN --no-check-certificate https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
+wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 
 # check installed #######################################
 if ! pacman -Q aria2 &>/dev/null; then
