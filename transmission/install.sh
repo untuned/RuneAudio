ebe5ceac0a56b363a1ab18cccc0081476b401bb0
rern <rernrern@gmail.com>
2017-08-24 20:23:00 +0700

    Update install.sh

--- transmission/install.sh
+++ transmission/install.sh
@@ -96,7 +96,7 @@ fi
 # web ui alternative
 if [[ $answebui == 1 ]]; then
 	wgetnc https://github.com/ronggang/transmission-web-control/raw/master/release/transmission-control-full.tar.gz
-	cp -rf /usr/share/transmission/web/* $path
+	mv /usr/share/transmission/web $path
 	mv $path/web/index{,.original.}.html
 	bsdtar -xf transmission-control-full.tar.gz -C $path
 	rm transmission-control-full.tar.gz
