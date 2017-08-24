1cfaef6212e8e6ce9f514f94c5486e73f7db1898
rern <rernrern@gmail.com>
2017-08-24 14:12:15 +0700

    Update setup.sh

--- _settings/setup.sh
+++ _settings/setup.sh
@@ -32,7 +32,7 @@ wgetnc $gitpath/motd/install.sh; chmod +x install.sh; ./install.sh
 touch /root/.hushlogin
 
 # passwords for samba and transmission
-echo -e "$bar root password for Samba and Transmission ...\n"
+echo -e "$bar root password for Samba and Transmission ..."
 setpwd
 
 echo -e "$bar Disable WiFi ..."
