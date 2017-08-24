f868038e5293ef9c7f432e8771942e4610beff1f
rern <rernrern@gmail.com>
2017-08-24 20:16:11 +0700

    Update install.sh

--- transmission/install.sh
+++ transmission/install.sh
@@ -71,7 +71,9 @@ ln -s /lib/systemd/system/trans{mission,}.service
 # create settings.json
 file=$path/settings.json
 [[ -e $file ]] && rm $file
-systemctl start trans; systemctl stop trans
+systemctl start trans
+sleep 1
+systemctl stop trans
 
 sed -i -e 's|"download-dir": ".*"|"download-dir": "'"$path"'"|
 ' -e 's|"incomplete-dir": ".*"|"incomplete-dir": "'"$path"'/incomplete"|
@@ -94,7 +96,7 @@ fi
 # web ui alternative
 if [[ $answebui == 1 ]]; then
 	wgetnc https://github.com/ronggang/transmission-web-control/raw/master/release/transmission-control-full.tar.gz
-	cp /usr/share/transmission/web $path
+	cp -rf /usr/share/transmission/web/* $path
 	mv $path/web/index{,.original.}.html
 	bsdtar -xf transmission-control-full.tar.gz -C $path
 	rm transmission-control-full.tar.gz
