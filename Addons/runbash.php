d0ea1fae2f78058f574b2dc4b0c2c407054d7252
rern <rernrern@gmail.com>
2017-08-28 21:00:53 +0700

    Create runbash.php

---
+++ Addons/runbash.php
@@ -0,0 +1,27 @@
+<?php
+
+$cmd = 'wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/aria2/install.sh; chmod +x install.sh; /usr/bin/sudo ./install.sh';
+//$cmd = '/usr/bin/sudo /usr/local/bin/uninstall_aria.sh';
+
+function bash($cmd) {
+	while (@ ob_end_flush());
+
+	$proc = popen("$cmd 2>&1", 'r');
+	$std = '';
+
+	echo '<pre>';
+	while (!feof($proc)) {
+		$std = fread($proc, 4096);
+		if (strpos($std, 'tput') === false || strpos($std, 'unknown terminal') === false) {
+			$std = preg_replace('/.\\[.*?m| \.  | i  /', '', $std);
+			$std = preg_replace('/ \.  | i  /', '', $std);
+			echo "$std";
+		}
+		@ flush();
+	}
+	echo '</pre>';
+
+	pclose($proc);
+}
+
+bash($cmd);
