e72957145a27280f723ec2b5dda84ca44a39e8bb
rern <rernrern@gmail.com>
2017-08-29 18:28:50 +0700

    Create install.sh

---
+++ font_extened/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+rm $0
+
+# import heading function
+wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
+
+if [[ -e /srv/http/assets/fonts/latobackup ]]; then
+	echo -e "$info Extended fonts already installed."
+	exit
+fi
+
+title -l = "$bar Install Extended fonts ..."
+wgetnc https://github.com/rern/_assets/RuneUI_enhancement/raw/master/lato.tar.xz
+mv /srv/http/assets/fonts/lato{,backup}
+mkdir /srv/http/assets/fonts/lato
+bsdtar -xvf lato.tar.xz -C /srv/http/assets/fonts/lato
+
+redis-cli hset addons font 1 &> /dev/null
+
+# refresh #######################################
+echo -e "$bar Clear PHP OPcache ..."
+curl '127.0.0.1/clear'
+echo
+
+if pgrep midori >/dev/null; then
+	killall midori
+	sleep 1
+	xinit &>/dev/null &
+	echo 'Local browser restarted.'
+fi
+
+title -l = "$bar Extended fonts installed successfully."
+echo 'Uninstall: uninstall_font.sh'
+title -nt "$info Refresh browser to start using."
