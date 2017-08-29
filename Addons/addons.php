0acb3fe8d244327555b87e69ee12c2babfd05732
rern <rernrern@gmail.com>
2017-08-29 19:06:51 +0700

    Update addons.php

--- Addons/addons.php
+++ Addons/addons.php
@@ -1,3 +1,19 @@
+<?php
+$redis = new Redis(); 
+$redis->pconnect('127.0.0.1');
+$addons = $redis->hGetAll('addons');
+$aria = $addons['aria'];
+$back = $addons['back'];
+$expa = $addons['expa'];
+$font = $addons['font'];
+$motd = $addons['motd'];
+$samb = $addons['samb'];
+$tran = $addons['tran'];
+$enha = $addons['enha'];
+$gpio = $addons['gpio'];
+$pass = $addons['pass'];
+$check = '<i class="fa fa-check blue"></i>';
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,105 +34,123 @@
 	#addons .btn {
 		text-transform: capitalize;
 	}
+	i {
+		margin-right: 10px;
+	}
 </style>
 </head>
 <body>
 
 <div id="addons" class="container">
-<h1>ADDONS</h1><a id="close"><i class="fa fa-times fa-lg"></i></a>
+<h1>ADDONS</h1><a id="close" href="/"><i class="fa fa-times fa-lg"></i></a>
 
 <div class="boxed-group">
-<legend>Aria2</legend>
+<legend><?php if ($aria) echo $check;?>Aria2</legend>
 <form class="form-horizontal">
-<p>Download utility that supports HTTP(S), FTP, BitTorrent, and Metalink. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/aria2/README.md">GitHub</a> )</p>
-<a id="inaria" class="btn btn-primary">Install</a> &nbsp; <a id="unaria" class="btn btn-default">Uninstall</a>
+<p>Download utility that supports HTTP(S), FTP, BitTorrent, and Metalink. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/aria2">GitHub</a> )</p>
+<a id="inaria" class="btn btn-default <?php if ($aria) echo 'disabled';?>">Install</a> &nbsp; <a id="unaria" class="btn btn-default <?php if (!$aria) echo 'disabled';?>">Uninstall</a>
 </form>
 </div>
 
 <div class="boxed-group">
-<legend>Backup-Restore Update</legend>
+<legend><?php if ($back) echo $check;?>Backup-Restore Update</legend>
 <form class="form-horizontal">
-<p>Enable backup-restore settings and databases. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/backup-restore/README.md">GitHub</a> )</p>
-<a id="inback" class="btn btn-primary">Install</a> &nbsp; <a id="unback" class="btn btn-default">Uninstall</a>
+<p>Enable backup-restore settings and databases. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/backup-restore">GitHub</a> )</p>
+<a id="inback" class="btn btn-default <?php if ($back) echo 'disabled';?>">Install</a> &nbsp; <a id="unback" class="btn btn-default <?php if (!$back) echo 'disabled';?>">Uninstall</a>
 </form>
 </div>
 
 <div class="boxed-group">
-<legend>Expand Partition</legend>
+<legend><?php if ($expa) echo $check;?>Expand Partition</legend>
 <form class="form-horizontal">
-<p>Expand default 2GB partition to full capacity of SD card. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/expand_partition/README.md">GitHub</a> )</p>
-<a id="inexpa" class="btn btn-primary">Install</a> &nbsp; <a id="unexpa" class="btn btn-default">Uninstall</a>
+<p>Expand default 2GB partition to full capacity of SD card. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/expand_partition">GitHub</a> )</p>
+<a id="inexpa" class="btn btn-default <?php if ($expa) echo 'disabled';?>">Expand</a>
 </form>
 </div>
 
 <div class="boxed-group">
-<legend>Extended character fonts</legend>
+<legend><?php if ($font) echo $check;?>Fonts - Extended characters</legend>
 <form class="form-horizontal">
-<p>Font files replacement for Cyrillic, Greek and IPA phonetics.</p>
-<a id="inexpa" class="btn btn-primary">Install</a> &nbsp; <a id="unexpa" class="btn btn-default">Uninstall</a>
+<p>Font files replacement for Cyrillic, Greek and IPA phonetics. ( More detail on <a href="https://github.com/rern/RuneAudio/tree/master/font_extended">GitHub</a> )</p>
+<a id="infont" class="btn btn-default <?php if ($font) echo 'disabled';?>">Install</a> &nbsp; <a id="unfont" class="btn btn-default <?php if (!$font) echo 'disabled';?>">Uninstall</a>
+</form>
+</div>
+
+<div class="boxed-group">
+<legend><?php if ($motd) echo $check;?>'motd' RuneAudio Logo for SSH Terminal</legend>
+<form class="form-horizontal">
+<p>Message of the day - RuneAudio Logo and dimmed command prompt. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/motd">GitHub</a> )</p>
+<a id="inmotd" class="btn btn-default <?php if ($motd) echo 'disabled';?>">Install</a> &nbsp; <a id="unmotd" class="btn btn-default <?php if (!$motd) echo 'disabled';?>">Uninstall</a>
 </form>
 </div>
 	
 <div class="boxed-group">
 <legend>Rank Mirror Packages Servers</legend>
 <form class="form-horizontal">
-<p>Fix packages download errors. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/rankmirrors/README.md">GitHub</a> )</p>
-<a id="inrank" class="btn btn-primary">Install</a> &nbsp; <a id="unrank" class="btn btn-default">Uninstall</a>
+<p>Fix packages download errors. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/rankmirrors">GitHub</a> )</p>
+<a id="inrank" class="btn btn-default">Rank</a>
 </form>
 </div>
 
 <div class="boxed-group">
-<legend>RuneAudio Logo for SSH Terminal</legend>
+<legend><?php if ($enha) echo $check;?>RuneUI Enhancements</legend>
 <form class="form-horizontal">
-<p>Message of the day - RuneAudio Logo and dimmed command prompt. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/motd/README.md">GitHub</a> )</p>
-<a id="inmotd" class="btn btn-primary">Install</a> &nbsp; <a id="unmotd" class="btn btn-default">Uninstall</a>
+<p>More minimalism and more fluid layout. ( More detail on <a href="https://github.com/rern/RuneUI_enhancement/blob/master">GitHub</a> )</p>
+<a id="inenha" class="btn btn-default <?php if ($enha) echo 'disabled';?>">Install</a> &nbsp; <a id="unenha" class="btn btn-default <?php if (!$enha) echo 'disabled';?>">Uninstall</a>
 </form>
 </div>
 
 <div class="boxed-group">
-<legend>RuneUI Enhancements</legend>
+<legend><?php if ($gpio) echo $check;?>RuneUI GPIO</legend>
 <form class="form-horizontal">
-<p>More minimalism and more fluid layout. ( More detail on <a href="https://github.com/rern/RuneUI_enhancement/blob/master/README.md">GitHub</a> )</p>
-<a id="inenha" class="btn btn-primary">Install</a> &nbsp; <a id="unenha" class="btn btn-default">Uninstall</a>
+<p>GPIO connected relay module control. ( More detail on <a href="https://github.com/rern/RuneUI_GPIO/blob/master">GitHub</a> )</p>
+<a id="ingpio" class="btn btn-default <?php if ($gpio) echo 'disabled';?>">Install</a> &nbsp; <a id="ungpio" class="btn btn-default <?php if (!$gpio) echo 'disabled';?>">Uninstall</a>
 </form>
 </div>
 
 <div class="boxed-group">
-<legend>RuneUI GPIO</legend>
+<legend><?php if ($pass) echo $check;?>RuneUI Password</legend>
 <form class="form-horizontal">
-<p>GPIO connected relay module control. ( More detail on <a href="https://github.com/rern/RuneUI_GPIO/blob/master/README.md">GitHub</a> )</p>
-<a id="ingpio" class="btn btn-primary">Install</a> &nbsp; <a id="ungpio" class="btn btn-default">Uninstall</a>
+<p>GPIO connected relay module control. ( More detail on <a href="https://github.com/rern/RuneUI_password/blob/master">GitHub</a> )</p>
+<a id="inpass" class="btn btn-default <?php if ($pass) echo 'disabled';?>">Install</a> &nbsp; <a id="unpass" class="btn btn-default <?php if (!$pass) echo 'disabled';?>">Uninstall</a>
 </form>
 </div>
 
 <div class="boxed-group">
-<legend>Samba Upgrade</legend>
+<legend><?php if ($samb) echo $check;?>Samba Upgrade</legend>
 <form class="form-horizontal">
-<p>Fast, easy, and free BitTorrent client. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/transmission/README.md">GitHub</a> )</p>
-<a id="intran" class="btn btn-primary">Install</a> &nbsp; <a id="untran" class="btn btn-default">Uninstall</a>
+<p>Fast, easy, and free BitTorrent client. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/transmission">GitHub</a> )</p>
+<a id="insamb" class="btn btn-default <?php if ($samb) echo 'disabled';?>">Install</a> &nbsp; <a id="unsamb" class="btn btn-default <?php if (!$samb) echo 'disabled';?>">Uninstall</a>
 </form>
 </div>
 	
 <div class="boxed-group">
-<legend>Transmission</legend>
+<legend><?php if ($tran) echo $check;?>Transmission</legend>
 <form class="form-horizontal">
-<p>Faster and more customized shares. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/samba/README.md">GitHub</a> )</p>
-<a id="intran" class="btn btn-primary">Install</a> &nbsp; <a id="untran" class="btn btn-default">Uninstall</a>
+<p>Faster and more customized shares. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/samba">GitHub</a> )</p>
+<a id="intran" class="btn btn-default <?php if ($tran) echo 'disabled';?>">Install</a> &nbsp; <a id="untran" class="btn btn-default <?php if (!$tran) echo 'disabled';?>">Uninstall</a>
 </form>
 </div>
 
 <div class="boxed-group">
 <legend>Webradio import</legend>
 <form class="form-horizontal">
-<p>Webradio files import script. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/webradio/README.md">GitHub</a> )</p>
-<a id="inwebr" class="btn btn-primary">Install</a> &nbsp; <a id="unwebr" class="btn btn-default">Uninstall</a>
+<p>Webradio files import script. ( More detail on <a href="https://github.com/rern/RuneAudio/blob/master/webradio">GitHub</a> )</p>
+<a id="inwebr" class="btn btn-default">Import</a>
 </form>
 </div>
 
 </div>
 <script src="assets/js/vendor/jquery-2.1.0.min.js"></script>
-<script src="assets/js/vendor/bootstrap.min.js"></script>
-<script src="assets/js/vendor/bootstrap-select-1.12.1.min.js"></script>
+<script>
+$(document).ready(function() {
+
+$('#addons a').click(function() {
+	window.location.href = '0.php?id='+ $(this).prop('id');
+});
+
+});
+</script>
 
 </body>
 </html>
