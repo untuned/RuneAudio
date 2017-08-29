dfe85eedebc2a0c16ec30062c0d945215f0b986c
rern <rernrern@gmail.com>
2017-08-29 10:31:50 +0700

    Update install.sh

--- backup-restore/install.sh
+++ backup-restore/install.sh
@@ -109,6 +109,7 @@ bsdtar -xpf $1 -C /
 systemctl start mpd redis
 mpc update Webradio
 hostnamectl set-hostname $( redis-cli get hostname )
+sed -i "s/opcache.enable=./opcache.enable=$( redis-cli get opcache )/" /etc/php/conf.d/opcache.ini
 rm $1
 ' > $file
 
