e5cda7a1c124c7becf14c978fd83ac7c5adb8057
rern <rernrern@gmail.com>
2017-08-29 19:54:39 +0700

    Update install.sh

--- Addons/install.sh
+++ Addons/install.sh
@@ -16,5 +16,27 @@ if grep -q 'Addons' /srv/http/app/templates/header.php; then
     exit
 fi
 
+wgetnc https://github.com/rern/RuneAudio/raw/master/Addons/runbash.sh
 wgetnc https://github.com/rern/RuneAudio/raw/master/Addons/uninstall_addo.sh -P /usr/local/bin
-chmod +x /usr/local/bin/uninstall_addo.sh
+chmod +x /srv/http/runbash.sh /usr/local/bin/uninstall_addo.sh
+
+sed -e '/poweroff-modal/ i\
+            <li><a href="/addons.php"><i class="fa fa-cubes"></i> Addons</a></li>
+' /srv/http/app/templates/header.php
+
+# refresh #######################################
+echo -e "$bar Clear PHP OPcache ..."
+curl '127.0.0.1/clear'
+echo
+
+if pgrep midori >/dev/null; then
+	killall midori
+	sleep 1
+	xinit &>/dev/null &
+	echo 'Local browser restarted.'
+fi
+
+timestop
+title -l = "$bar Addons menu installed successfully."
+echo 'Uninstall: uninstall_addo.sh'
+title -nt "$info Refresh browser and go to Menu > Addons."
