3759ede26811cdfa0d599850293857c3e7b22ca0
rern <rernrern@gmail.com>
2017-08-29 13:23:22 +0700

    Update install.sh

--- motd/install.sh
+++ motd/install.sh
@@ -62,5 +62,7 @@ PS1=\x27\\[\\e[38;5;\x27$color\x27m\\]\\u@\\h:\\[\\e[0m\\]\\w \\$ \x27
 # \w         - current directory
 # \$         - promt symbol: <$> users; <#> root
 
+redis-cli hset addons motd 1
+
 echo -e "\nUninstall: uninstall_motd.sh"
 title -nt "$info Relogin to see new Rune logo motd."
