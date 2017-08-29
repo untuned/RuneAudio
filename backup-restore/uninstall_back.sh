c3b8db7483810c0d37be64399390c023961b3851
rern <rernrern@gmail.com>
2017-08-29 14:05:07 +0700

    Update uninstall_back.sh

--- backup-restore/uninstall_back.sh
+++ backup-restore/uninstall_back.sh
@@ -27,7 +27,7 @@ sed -i 's/\$("#restore").\+});//' /srv/http/assets/js/runeui.min.js
 
 rm /srv/http/restore.* /etc/sudoers.d/http-backup
 
-redis-cli hset addons back 0
+redis-cli hset addons back 0 &> /dev/null
 systemctl restart rune_SY_wrk
 
 echo -e "$bar Clear PHP OPcache ..."
