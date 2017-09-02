4665e70bc4fb71b16a1ad61da24a64ff022e6ce7
rern <rernrern@gmail.com>
2017-09-02 20:03:54 +0700

    Create install.sh

---
+++ samba/install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+version=20170901
+
+rm $0
+
+# import heading function
+wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
+
+if [[ $( smbd -V ) != 'Version xxxx' ]]; then
+	echo -e "$info Samba already upgraged."
+	exit
+fi
+
+title -l = "$bar Upgrade Samba ..."
+#################################################################################
+timestart
+
+# fix packages download errors
+if  grep '^Server = http://mirror.archlinuxarm.org/' /etc/pacman.d/mirrorlist; then
+	wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/rankmirrors/rankmirrors.sh
+	chmod +x rankmirrors.sh
+	./rankmirrors.sh
+fi
+pacman -Sy
+
+systemctl stop nmbd smbd
+
+pacman -R --noconfirm samba4-rune
+pacman -S --noconfirm tdb tevent smbclient samba
+pacman -S --noconfirm libwbclient
+
+# fix 'minimum rlimit_max'
+echo -n '
+root    soft    nofile    16384
+root    hard    nofile    16384
+' >> /etc/security/limits.conf
+
+wgetnc https://github.com/rern/RuneAudio/raw/master/samba/smb-dev.conf -O /etc/samba/smb-dev.conf
+ln -sf /etc/samba/smb{-dev,}.conf
+
+# set samba password
+(echo $1; echo $1) | smbpasswd -s -a root
+
+systemctl daemon-reload
+systemctl restart nmbd smbd
+
+timestop
+title -l = "$bar Samba upgraded successfully."
+title -nt "$info Edit /etc/smb-dev.conf to fit usage."
