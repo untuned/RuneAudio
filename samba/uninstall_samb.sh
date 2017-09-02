edfc1b106adae7c1c66ac80189e7e99ee6749b85
rern <rernrern@gmail.com>
2017-09-02 20:13:00 +0700

    Update uninstall_samb.sh

--- samba/uninstall_samb.sh
+++ samba/uninstall_samb.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# import heading function
-wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
-
-echo -e "$info Samba cannot be downgraged."
+echo -e "\e[38;5;0m\e[48;5;3m i \e[0m Samba cannot be downgraged."
