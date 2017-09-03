128e50398330d05fb3e0ef5864bc8fa8ce944695
rern <rernrern@gmail.com>
2017-09-03 21:57:31 +0700

    Update install.sh

--- samba/install.sh
+++ samba/install.sh
@@ -21,8 +21,7 @@ gitpath=https://github.com/rern/RuneAudio/raw/master
 if  grep '^Server = http://mirror.archlinuxarm.org/' /etc/pacman.d/mirrorlist; then
 	wgetnc $gitpath/rankmirrors/rankmirrors.sh
 	chmod +x rankmirrors.sh
-	./rankmirrors.sh
-	rm rankmirrors.sh
+	./rankmirrors.sh 1
 fi
 pacman -Sy
 
