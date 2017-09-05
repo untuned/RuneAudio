138607c9f5e315acda148305c4ad8dbe60ca2a91
rern <rernrern@gmail.com>
2017-09-05 16:06:01 +0700

    Update uninstall_aria.sh

--- aria2/uninstall_aria.sh
+++ aria2/uninstall_aria.sh
@@ -12,24 +12,26 @@ fi
 title -l = "$bar Uninstall Aria2 ..."
 systemctl disable aria2
 systemctl stop aria2
-rm /etc/systemd/system/aria2.service
+rm -v /etc/systemd/system/aria2.service
 systemctl daemon-reload
 # uninstall package #######################################
 pacman -Rs --noconfirm aria2
 
 # restore file
+file=/etc/nginx/nginx.conf
+echo $file
 sed -i -e '/location \/aria2/, /^$/ d
 ' -e '/^\s*rewrite/ d
 ' -e 's/#rewrite/rewrite/g
-' /etc/nginx/nginx.conf
+' $file
 
 # remove files #######################################
 echo -e "$bar Remove files ..."
 if mount | grep -q '/dev/sda1'; then
 	mnt=$( mount | grep '/dev/sda1' | awk '{ print $3 }' )
-	rm -r $mnt/aria2/web
+	rm -rv $mnt/aria2/web
 else
-	rm -r /root/aria2/web
+	rm -rv /root/aria2/web
 fi
 rm -rv /root/.config/aria2 /srv/http/aria2
 
