fe75119155b8d5db570b3411888c876b360cc63a
rern <rernrern@gmail.com>
2017-09-11 20:49:10 +0700

    Update uninstall_tran.sh

--- transmission/uninstall_tran.sh
+++ transmission/uninstall_tran.sh
@@ -5,7 +5,6 @@ wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; r
 
 # if update, save settings
 if [[ ${@:$#} == -u ]]; then
-	update=1
 	if mount | grep -q '/dev/sda1'; then
 		mnt=$( mount | grep '/dev/sda1' | awk '{ print $3 }' )
 		mkdir -p $mnt/transmission
