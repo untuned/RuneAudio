a2d3b8af1562dfa53df9951a7a48dadda577ecbc
rern <rernrern@gmail.com>
2017-09-11 21:08:23 +0700

    Update uninstall_tran.sh

--- transmission/uninstall_tran.sh
+++ transmission/uninstall_tran.sh
@@ -3,6 +3,14 @@
 # import heading function
 wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 
+# check installed #######################################
+if ! pacman -Q transmission-cli &>/dev/null; then
+	echo -e "$info Transmission not found."
+	exit 1
+fi
+
+title -l = "$bar Uninstall Transmission ..."
+
 if mount | grep -q '/dev/sda1'; then
 	mnt=$( mount | grep '/dev/sda1' | awk '{ print $3 }' )
 	mkdir -p $mnt/transmission
@@ -12,22 +20,15 @@ else
 	path=/root/transmission
 fi
 
-# if update, save settings
+# if update, save settings #######################################
 if [[ ${@:$#} == -u ]]; then
 	rm -r /tmp/tran
 	mkdir -p /tmp/tran
-	mv $path/settings.json /tmp/tran
+	cp $path/settings.json /tmp/tran
 	[[ -e $path/web ]] && touch /tmp/tran/answebui
 	[[ -e /etc/systemd/system/multi-user.target.wants/transmission.service ]] && touch /tmp/tran/ansstartup
 fi
 
-# check installed #######################################
-if ! pacman -Q transmission-cli &>/dev/null; then
-	echo -e "$info Transmission not found."
-	exit 1
-fi
-
-title -l = "$bar Uninstall Transmission ..."
 # uninstall package #######################################
 pacman -Rs --noconfirm transmission-cli
 
