11c1b33a5eb1915c9a83ce65898a123d6f1f8228
rern <rernrern@gmail.com>
2017-09-11 20:46:01 +0700

    Update uninstall_tran.sh

--- transmission/uninstall_tran.sh
+++ transmission/uninstall_tran.sh
@@ -3,6 +3,22 @@
 # import heading function
 wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 
+# if update, save settings
+if [[ ${@:$#} == -u ]]; then
+	update=1
+	if mount | grep -q '/dev/sda1'; then
+		mnt=$( mount | grep '/dev/sda1' | awk '{ print $3 }' )
+		mkdir -p $mnt/transmission
+		path=$mnt/transmission
+	else
+		mkdir -p /root/transmission
+		path=/root/transmission
+	fi
+	mv $path/settings.json{,.upgrade}
+	[[ -e $path/web ]] && answebui=1
+	[[ -e /etc/systemd/system/multi-user.target.wants/transmission.service ]] && ansstartup=1
+fi
+
 # check installed #######################################
 if ! pacman -Q transmission-cli &>/dev/null; then
 	echo -e "$info Transmission not found."
@@ -18,13 +34,7 @@ echo -e "$bar Remove files ..."
 
 rm -rv /etc/systemd/system/transmission.service.d
 rm -v /lib/systemd/system/trans.service
-
-if mount | grep -q '/dev/sda1'; then
-	mnt=$( mount | grep '/dev/sda1' | awk '{ print $3 }' )
-	rm -r $mnt/transmission/web
-else
-	rm -r /root/transmission/web
-fi
+rm -r $path/web
 
 redis-cli hdel addons tran &> /dev/null
 
