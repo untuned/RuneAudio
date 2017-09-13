30f11aafa70ba9c69bda5d33f3f6c83980286aa3
rern <rernrern@gmail.com>
2017-09-13 21:25:09 +0700

    Update install.sh

--- font_extended/install.sh
+++ font_extended/install.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-version=20170901
-
-rm $0
+# required variables
+alias=font
+title='Fonts - Extended Characters'
 
 # import heading function
 wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 
-if [[ -e /usr/local/bin/uninstall_font.sh ]]; then
-	echo -e "$info Extended fonts already installed."
-	exit
-fi
-
-[[ $1 != u ]] && title -l = "$bar Install Extended fonts ..."
+installstart
 
 wgetnc https://github.com/rern/RuneAudio/raw/master/font_extended/uninstall_font.sh -P /usr/local/bin
 chmod +x /usr/local/bin/uninstall_font.sh
@@ -22,22 +17,11 @@ mkdir /srv/http/assets/fonts/lato
 bsdtar -xvf lato.tar.xz -C /srv/http/assets/fonts/lato
 rm lato.tar.xz
 
-redis-cli hset addons font $version &> /dev/null
+installfinish
 
-if [[ $1 != u ]]; then
-	title -l = "$bar Extended fonts installed successfully."
-	[[ -t 1 ]] && echo 'Uninstall: uninstall_font.sh'
-	title -nt "$info Refresh browser for new fonts."
-else
-	title -l = "$bar Extended fonts updated successfully."
-fi
+title -nt "$info Refresh browser for new fonts."
+
+restartmidori
 
 # clear opcache if run from terminal #######################################
 [[ -t 1 ]] && systemctl reload php-fpm
-
-# restart local browser #######################################
-if pgrep midori > /dev/null; then
-	killall midori
-	sleep 1
-	xinit &> /dev/null &
-fi
