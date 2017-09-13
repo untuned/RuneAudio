9dfd07a29eaef4e1e3973b5ac327ff130244d3be
rern <rernrern@gmail.com>
2017-09-13 21:37:45 +0700

    Update install.sh

--- font_extended/install.sh
+++ font_extended/install.sh
@@ -7,7 +7,7 @@ title='Fonts - Extended Characters'
 # import heading function
 wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 
-installstart
+installstart $1
 
 wgetnc https://github.com/rern/RuneAudio/raw/master/font_extended/uninstall_font.sh -P /usr/local/bin
 chmod +x /usr/local/bin/uninstall_font.sh
@@ -17,7 +17,7 @@ mkdir /srv/http/assets/fonts/lato
 bsdtar -xvf lato.tar.xz -C /srv/http/assets/fonts/lato
 rm lato.tar.xz
 
-installfinish
+installfinish $1
 
 title -nt "$info Refresh browser for new fonts."
 
