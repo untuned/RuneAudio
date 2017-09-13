5c1cb30673d5d949898fd9cd519f683542d0911d
rern <rernrern@gmail.com>
2017-09-13 21:40:34 +0700

    Update uninstall_tran.sh

--- transmission/uninstall_tran.sh
+++ transmission/uninstall_tran.sh
@@ -7,7 +7,7 @@ title='Transmission'
 # import heading function
 wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 
-uninstallstart
+uninstallstart $1
 
 if mount | grep -q '/dev/sda1'; then
 	mnt=$( mount | grep '/dev/sda1' | awk '{ print $3 }' )
@@ -33,4 +33,4 @@ rm -rv /etc/systemd/system/transmission.service.d
 rm -v /lib/systemd/system/trans.service
 rm -r $path/web
 
-uninstallfinish
+uninstallfinish $1
