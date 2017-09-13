d166a4294577af50f2167dd2d8b4c2339636568e
rern <rernrern@gmail.com>
2017-09-13 21:37:39 +0700

    Update uninstall_font.sh

--- font_extended/uninstall_font.sh
+++ font_extended/uninstall_font.sh
@@ -1,41 +1,17 @@
 #!/bin/bash
 
+# required variables
+alias=font
+title='Fonts - Extended Characters'
+
 # import heading function
 wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 
-if [[ ! -e /usr/local/bin/uninstall_font.sh ]]; then
-	echo -e "$info Extended fonts not found."
-	exit 1
-fi
-
-[[ $1 != u ]] && type=Uninstall || type=Update
-title -l = "$bar $type Extended fonts ..."
+uninstallstart $1
 
 rm -rv /srv/http/assets/fonts/lato
 mv -v /srv/http/assets/fonts/lato{.backup,}
 
-redis-cli hdel addons font &> /dev/null
+uninstallfinish $1
 
-if pgrep midori >/dev/null; then
-	killall midori
-	sleep 1
-	xinit &>/dev/null &
-	echo 'Local browser restarted.'
-fi
-
-if [[ $1 != u ]]; then
-	title -l = "$bar Extended fonts uninstalled successfully."
-	title -nt "$info Refresh browser for original fonts."
-fi
-
-# clear opcache if run from terminal #######################################
-[[ -t 1 ]] && systemctl reload php-fpm
-
-# restart local browser #######################################
-if pgrep midori > /dev/null; then
-	killall midori
-	sleep 1
-	xinit &> /dev/null &
-fi
-
-rm $0
+clearcache
