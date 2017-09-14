094ff2c494f60b93984a65742c3468bd763878f0
rern <rernrern@gmail.com>
2017-09-14 20:07:55 +0700

    Update expand.sh

--- expand_partition/expand.sh
+++ expand_partition/expand.sh
@@ -9,7 +9,7 @@ rm $0
 wget -qN https://github.com/rern/RuneAudio_Addons/raw/master/title.sh; . title.sh; rm title.sh
 
 if [[ ! -e /usr/bin/sfdisk ]] || [[ ! -e /usr/bin/python2 ]]; then
-	echo -e "$info Unable to continue with this version."
+	title -l '=' "$info Unable to continue with this version."
 	echo "(sfdisk and python2 expected but not found.)"
 	exit
 fi
@@ -26,12 +26,12 @@ unpartmb=$( python2 -c "print($unpartb / 1000000)" )
 summb=$(( $freemb + $unpartmb ))
 # noobs has 3MB unpartitioned space
 if [[ $unpartmb -lt 10 ]]; then
-	echo -e "$info No useful space available. ( ${unpartmb}MB unused)"
+	title -l '=' "$info No useful space available. ( ${unpartmb}MB unused)"
 	redis-cli hset addons expa 1 &> /dev/null
 	exit
 fi
 
-if ls /dev/sd* &>/dev/null; then
+if [[ -t 1 ]] && ls /dev/sd* &>/dev/null; then
 	echo -e "$info Unmount and remove all USB drives before proceeding:"
 	hdd=$( ls /dev/sd? )
 	echo -e "\e[0;36m$hdd\e[m"
@@ -43,12 +43,14 @@ if ls /dev/sd* &>/dev/null; then
 fi
 
 # expand partition #######################################
-title -l = $bar Expand partition ...
+title -l '=' "$bar Expand partition ..."
 echo -e "Current partiton: \e[0;36m$devpart\e[m"
 echo -e "Available free space \e[0;36m$freemb MB\e[m"
 echo -e "Available unused disk space: \e[0;36m$unpartmb MB\e[m"
 echo
-yesno "Expand partiton to full unused space:" answer
+
+[[ -t 1 ]] && yesno "Expand partiton to full unused space:" answer || answer=1
+
 if [[ $answer == 1 ]]; then
 	if ! pacman -Q parted &>/dev/null; then
 		echo -e "$bar Get package file ..."
