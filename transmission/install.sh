2c83f8c20daa710361dd8320bb53af1a15b58ea5
rern <rernrern@gmail.com>
2017-09-14 11:34:58 +0700

    Update install.sh

--- transmission/install.sh
+++ transmission/install.sh
@@ -4,7 +4,7 @@
 alias=tran
 
 # import heading function
-wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
+wget -qN https://github.com/rern/RuneAudio_Addons/raw/master/title.sh; . title.sh; rm title.sh
 
 # user inputs
 if (( $# == 0 )); then # with no argument
