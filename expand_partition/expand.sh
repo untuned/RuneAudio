fbf25dba2f5dde3cdb79a6b91210c2f0b530290e
rern <rernrern@gmail.com>
2017-09-15 09:40:02 +0700

    Update expand.sh

--- expand_partition/expand.sh
+++ expand_partition/expand.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# expand.sh - expand partition
-# https://github.com/rern/expand_partition
-
 rm $0
 
-# import heading function
 wget -qN https://github.com/rern/RuneAudio_Addons/raw/master/title.sh; . title.sh; rm title.sh
 
 if [[ ! -e /usr/bin/sfdisk ]] || [[ ! -e /usr/bin/python2 ]]; then
@@ -14,7 +10,6 @@ if [[ ! -e /usr/bin/sfdisk ]] || [[ ! -e /usr/bin/python2 ]]; then
 	exit
 fi
 
-# partition data #######################################
 freekb=$( df | grep '/$' | awk '{print $4}' ) # free disk space in kB
 freemb=$( python2 -c "print($freekb / 1000)" ) # bash itself cannot do float
 devpart=$( mount | grep 'on / type' | awk '{print $1}' )
@@ -33,8 +28,7 @@ fi
 
 if [[ -t 1 ]] && ls /dev/sd* &>/dev/null; then
 	echo -e "$info Unmount and remove all USB drives before proceeding:"
-	hdd=$( ls /dev/sd? )
-	echo -e "\e[0;36m$hdd\e[m"
+	ls /dev/sd?
 	echo
 	read -n 1 -s -p 'Press any key to continue ... '
 	echo
@@ -42,16 +36,17 @@ fi
 
 # expand partition #######################################
 title -l '=' "$bar Expand partition ..."
-echo -e "Current partiton: \e[0;36m$devpart\e[m"
-echo -e "Available free space \e[0;36m$freemb MB\e[m"
-echo -e "Available unused disk space: \e[0;36m$unpartmb MB\e[m"
+echo "Current partiton           : $( tcolor $devpart )"
+echo "Available free space       : $( tcolor $freemb MB )"
+echo "Available unused disk space: $( tcolor $unpartmb MB )"
 echo
 
-[[ -t 1 ]] && yesno "Expand partiton to full unused space:" answer
-
-if [[ -t 1 && $answer == 0 ]]; then
-	title "$info Expand partition cancelled."
-	exit
+if [[ -t 1 ]]; then
+	yesno "Expand partiton to full unused space:" answer
+	if [[ $answer == 0 ]]; then
+		title "$info Expand partition cancelled."
+		exit
+	fi
 fi
 
 if ! pacman -Q parted &>/dev/null; then
@@ -69,13 +64,13 @@ partprobe $disk
 resize2fs $devpart
 	
 if [[ $? != 0 ]]; then
-	echo -e "$warn Failed: Expand partition\nTry - reboot > resize2fs $devpart"
+	title -l '=' "$warn Expand partition failed.\nTry - reboot > resize2fs $devpart"
 	exit
 else
 	freekb=$( df | grep '/$' | awk '{print $4}' )
 	freemb=$( python2 -c "print($freekb / 1000)" )
 	echo
-	# expanded marking
-	redis-cli hset addons expa 1 &> /dev/null
-	title -l '=' "$bar Partiton \e[0;36m$devpart\e[m now has \e[0;36m$freemb\e[m MB free space."
+	
+	redis-cli hset addons expa 1 &> /dev/null # mark as expanded - disable webui button
+	title -l '=' "$bar Partiton $( tcolor $devpart ) now has $( tcolor $freemb ) MB free space."
 fi
