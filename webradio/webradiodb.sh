d887e062f68fa3fb728fd41c6deb960cee2b3567
rern <rernrern@gmail.com>
2017-09-23 08:33:26 +0700

    Update webradiodb.sh

--- webradio/webradiodb.sh
+++ webradio/webradiodb.sh
@@ -6,7 +6,7 @@
 # - refresh ui
 # - fix sorting
 
-rm $0
+#rm $0
 
 [[ ! -e /srv/http/addonstitle.sh ]] && wget -q https://github.com/rern/RuneAudio_Addons/raw/master/srv/http/addonstitle.sh -P /srv/http
 . /srv/http/addonstitle.sh
@@ -37,7 +37,7 @@ mpc update Webradio &> /dev/null
 # fix sorting
 runeui=/srv/http/assets/js/runeui.js
 if ! grep '^//\s*if (path === '"'"'Webradio'"'"')' $runeui; then
-    sed -i $'/^\s+if (path === \'Webradio\')/, /}/ s|^|//|' $runeui
+    sed -i $'/^\s*if (path === \'Webradio\')/, /}/ s|^|//|' $runeui
 fi
 
 if ! grep -q 'localeCompare\|addwebradio' $runeui; then
@@ -47,19 +47,20 @@ if ! grep -q 'localeCompare\|addwebradio' $runeui; then
             });\
             $("#database-entries").append(elems);\
 	    if (path === "Webradio") {\
-	        var addwebradio = '<li id="webradio-add" class="db-webradio-add"><i class="fa fa-plus-circle db-icon"></i><span class="sn"><em>add new</em></span><span class="bl">add a webradio to your library</span></li>';\
+	        var addwebradio = '"'"'<li id="webradio-add" class="db-webradio-add"><i class="fa fa-plus-circle db-icon"></i><span class="sn"><em>add new</em></span><span class="bl">add a webradio to your library</span></li>'"'"';\
                 $("#database-entries").append(addwebradio);\
             }
     ' $runeui
     
     sed -i 's/var u=$("span","#db-currentpath")/var elems=$("#database-entries li").detach().sort(function(a,b){return $(a).text().toLowerCase().localeCompare($(b).text().toLowerCase())});$("#database-entries").append(elems);&/
     ' ${runeui/.js/.min.js}
-else
+fi
+if ! grep -q 'var addwebradio' $runeui; then
     sed -i '/("#database-entries")\.append(elems)/ a\
-            if (path === "Webradio") {\
-                var addwebradio = '<li id="webradio-add" class="db-webradio-add"><i class="fa fa-plus-circle db-icon"></i><span class="sn"><em>add new</em></span><span class="bl">add a webradio to your library</span></li>';\
-                $("#database-entries").append(addwebradio);\
-            }
+			if (path === "Webradio") {\
+				var addwebradio = '"'"'<li id="webradio-add" class="db-webradio-add"><i class="fa fa-plus-circle db-icon"></i><span class="sn"><em>add new</em></span><span class="bl">add a webradio to your library</span></li>'"'"';\
+				$("#database-entries").append(addwebradio);\
+			}
     ' $runeui
 fi
 
