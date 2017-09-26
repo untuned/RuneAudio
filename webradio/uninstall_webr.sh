9e2bcb7c637057ad2279e6481881a7cf11f38d84
rern <rernrern@gmail.com>
2017-09-26 12:33:56 +0700

    Update uninstall_webr.sh

--- webradio/uninstall_webr.sh
+++ webradio/uninstall_webr.sh
@@ -9,17 +9,17 @@ uninstallstart $1
 file=/srv/http/assets/js/runeui.js
 echo $file
 
-if grep -q "^//\s*if (path === 'Webradio')" $file; then
-    sed -i $'/^\s*if (path === \'Webradio\')/, /}/ s|^//||' $file
-fi
-
 if grep -q 'var addwebradio' $file; then
-    sed -i '/if (path === "Webradio")/,/}/ d' $file
+    sed -i '/^\t*\s*if (path === "Webradio")/,/}/ d' $file
     
 	file=/srv/http/assets/js/runeui.min.js
 	echo $file
 	perl -p -i -e 's|/\*("Webradio"===t.*?</li>'"'"'\),)\*/|\1|' $file
 	perl -p -i -e 's|if\("Webradio"===path\).*?(var u=\$\("span","#db-currentpath"\))|\1|' $file
+fi
+
+if grep -q "^//\t*\s*if (path === 'Webradio')" $file; then
+    sed -i $'|^//\t*\s*if (path === \'Webradio\')|, |}| s|^//||' $file
 fi
 
 uninstallfinish $1
