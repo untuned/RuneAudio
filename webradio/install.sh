1afef6d9760b7ce5d9b58193451d538d82c70aad
rern <rernrern@gmail.com>
2017-09-26 12:41:17 +0700

    Update install.sh

--- webradio/install.sh
+++ webradio/install.sh
@@ -27,14 +27,14 @@ sed -i $'/^\s*if (path === \'Webradio\')/, /}/ s|^|//|' $runeui
 
 if ! grep -q 'var addwebradio' $runeui; then
     sed -i '/highlighted entry/ a\
-			if (path === "Webradio") {\
+			if (path === "Webradio") { // webr\
 				var elems = $("#database-entries li").detach().sort(function (a, b) {\
 					return $(a).text().toLowerCase().localeCompare($(b).text().toLowerCase());\
 				});\
 				$("#database-entries").append(elems);\
 				var addwebradio = '"'"'<li id="webradio-add" class="db-webradio-add"><i class="fa fa-plus-circle db-icon"></i><span class="sn"><em>add new</em></span><span class="bl">add a webradio to your library</span></li>'"'"';\
 				$("#database-entries").append(addwebradio);\
-			}
+			} // webr
     ' $runeui
 fi
 
