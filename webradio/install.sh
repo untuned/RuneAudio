65b361336e41e0b3183e5f0ef22adffc24dcbeac
rern <rernrern@gmail.com>
2017-09-27 20:55:07 +0700

    Update install.sh

--- webradio/install.sh
+++ webradio/install.sh
@@ -21,18 +21,18 @@ sed -i '/("#database-entries li").detach()/,/("#database-entries").append(elems)
 sed -i 's/var elems=.*("span","#db-currentpath")/var u=$("span","#db-currentpath")/' $runeuimin
 
 # modify files
-sed -i $'/^\s*if (path === \'Webradio\')/, /}/ s|^|// webr|' $runeui
+sed -i $'/^\s*if (path === \'Webradio\')/, /}/ s|^|//webr|' $runeui
 
 if ! grep -q 'var addwebradio' $runeui; then
     sed -i '/highlighted entry/ a\
-			if (path === "Webradio") { // webr0\
+			if (path === "Webradio") { //webr0\
 				var elems = $("#database-entries li").detach().sort(function (a, b) {\
 					return $(a).text().toLowerCase().localeCompare($(b).text().toLowerCase());\
 				});\
 				$("#database-entries").append(elems);\
 				var addwebradio = '"'"'<li id="webradio-add" class="db-webradio-add"><i class="fa fa-plus-circle db-icon"></i><span class="sn"><em>add new</em></span><span class="bl">add a webradio to your library</span></li>'"'"';\
 				$("#database-entries").append(addwebradio);\
-			} // webr1
+			} //webr1
     ' $runeui
 fi
 
