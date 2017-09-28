0d5dec13b49c78264db3aec52187bb9454317420
rern <rernrern@gmail.com>
2017-09-28 23:21:23 +0700

    Update install.sh

--- font_extended/install.sh
+++ font_extended/install.sh
@@ -4,7 +4,6 @@
 
 alias=font
 
-[[ ! -e /srv/http/addonstitle.sh ]] && wget -q https://github.com/rern/RuneAudio_Addons/raw/master/srv/http/addonstitle.sh -P /srv/http
 . /srv/http/addonstitle.sh
 
 installstart $1
