2008770dbc6f7529b36148fc8991824c1b0ffc9e
rern <rernrern@gmail.com>
2017-10-09 20:12:13 +0700

    Update uninstall_spla.sh

--- boot_splash/uninstall_spla.sh
+++ boot_splash/uninstall_spla.sh
@@ -8,21 +8,17 @@ uninstallstart $1
 
 echo -e "$bar Restore files ..."
 echo /boot/cmdline.txt
-sed -i -e 's/console=tty3/console=ttyAMA0,115200 console=tty1/
-' -e 's/ loglevel=0 logo.nologo//
-' /boot/cmdline.txt
+sed -i -e 's/ loglevel=0 logo.nologo//' /boot/cmdline.txt
 
 echo /boot/config.txt
 sed '/disable_splash=1/ d' /boot/confix.txt
 
-mv /etc/motd{.ply,}
-
 systemctl disable ply-image
+systemctl enable getty@tty1.service
 
 echo -e "$bar Remove files ..."
 rm -v /etc/systemd/system/ply-image.service
 rm -v /usr/local/bin/ply-image
 rm -rv /usr/share/ply-image
-rm -v /root/.hushlogin
 
 uninstallfinish $1
