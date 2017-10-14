3be92c2b25932c4f977133b89cd463bc7a095091
rern <rernrern@gmail.com>
2017-10-14 20:49:30 +0700

    Update cmd.sh

--- _settings/cmd.sh
+++ _settings/cmd.sh
@@ -57,16 +57,19 @@ bootrune() {
 }
 
 setup() {
-	if [[ ! -e /etc/motd.logo ]]; then
-		echo -e "\e[38;5;6m\e[48;5;6m . \e[0m Set date-time ..."
-		systemctl stop ntpd
-		ntpdate pool.ntp.org
-		systemctl start ntpd
+	if [[ -e /usr/local/bin/uninstall_addo.sh ]]; then
+		echo -e "\e[30m\e[43m ! \e[0m Already setup."
+	else
 		wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/_settings/setup.sh
+		if [[ $? == 5 ]]; then
+			echo -e "\e[38;5;6m\e[48;5;6m . \e[0m Sync time ..."
+			systemctl stop ntpd
+			ntpdate pool.ntp.org
+			systemctl start ntpd
+			wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/_settings/setup.sh
+		fi
 		chmod +x setup.sh
 		./setup.sh
-	else
-		echo -e "\e[30m\e[43m ! \e[0m Already setup."
 	fi
 }
 resetosmc() {
