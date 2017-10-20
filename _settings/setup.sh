13202878ea733ca19380e080d9ecae5b549e72fc
rern <rernrern@gmail.com>
2017-10-20 20:08:10 +0700

    Update setup.sh

--- _settings/setup.sh
+++ _settings/setup.sh
@@ -181,6 +181,11 @@ echo
 wgetnc https://github.com/rern/RuneUI_enhancement/raw/master/install.sh; chmod +x install.sh; ./install.sh 1.5
 echo
 
+# Library breadcrumb
+#################################################################################
+wgetnc $gitpath/breadcrumb/install.sh; chmod +x install.sh; ./install.sh
+echo
+
 # GPIO
 #################################################################################
 wgetnc $gitpath/_settings/mpd.conf.gpio -P /etc
