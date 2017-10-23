b20bcfa3de7e198d6de8f10b8c459d4ef703eb90
rern <rernrern@gmail.com>
2017-10-23 11:23:06 +0700

    Update install.sh

--- mpd/install.sh
+++ mpd/install.sh
@@ -22,29 +22,32 @@ chmod 755 /usr/lib/{libcrypto.so.1.1,libssl.so.1.1}
 cp /etc/mpd.conf{,.backup}
 
 echo -e "$bar Remove conflict packages ..."
-pacman -R --noconfirm ashuffle-rune mpd-rune ffmpeg-rune
+pacman -R --noconfirm ashuffle-rune ffmpeg-rune mpd-rune
 
 rankmirrors
 
 echo -e "$bar Install packages ..."
-# fix libreadline error
-pacman -S --noconfirm readline
-ln -s /lib/libreadline.so.7.0 /lib/libreadline.so.6
-
-pacman -S --noconfirm base-devel
-pacman -S --noconfirm alsa-lib audiofile avahi boost bzip2 curl dbus doxygen expat faad2 ffmpeg flac guile icu jack lame ldb 
-pacman -S --noconfirm libao libcdio-paranoia libgme libid3tag libmad libmms libmodplug libmpdclient libnfs libogg libsamplerate libshout libsndfile libsoxr libupnp libutil-linux libvorbis libwebp
-pacman -S --noconfirm mp3unicode mpg123 smbclient sqlite tdb tevent wavpack yajl zlib zziplib
+pacman -S --noconfirm libnfs icu libwebp gcc-libs wavpack
 
 echo -e "$bar Install MPD ..."
 pacman -S --noconfirm mpd
-cp /etc/mpd.conf{.backup,}
+systemctl stop mpd
+
 wgetnc https://github.com/rern/RuneAudio/raw/master/mpd/ashuffle-rune-1.0-20160319-armv7h.pkg.tar.xz
 pacman -U ashuffle-rune-1.0-20160319-armv7h.pkg.tar.xz
 rm ashuffle-rune-1.0-20160319-armv7h.pkg.tar.xz
 
+cp /etc/mpd.conf{.backup,}
+
+sed -i -e '/^Protect/ s/^/#/
+' -e '/^Restrict/ s/^/#/
+' /usr/lib/systemd/system/mpd.service
+
+touch /var/log/mpd.log
+chmod 777 /var/log/mpd.log
+
 systemctl daemon-reload
-systemctl restart mpd
+systemctl start mpd
 
 redis-cli hset addons mpdu 1 &> /dev/null # mark as upgraded - disable button
 
