cea761b44521c5743c47bc6cd918b0cfc2273ec8
rern <rernrern@gmail.com>
2017-10-24 20:28:21 +0700

    Update install.sh

--- mpd/install.sh
+++ mpd/install.sh
@@ -64,9 +64,9 @@ ln -s /usr/lib/libicui18n.so.59.1 /usr/lib/libicui18n.so.56
 ln -s /usr/lib/libicuuc.so.59.1 /usr/lib/libicuuc.so.56
 ln -s /usr/lib/libwebp.so.7.0.0 /usr/lib/libwebp.so.6
 ln -s /usr/lib/libicudata.so.59.1 /usr/lib/libicudata.so.56
-pacman -S --noconfirm glib2 gtk3 webkitgtk
+pacman -S --noconfirm webkitgtk gtk3 glib2
 
-[[ $midori == 1 ]] && sleep 1 && xinit &> /dev/null &
+[[ $midori == 1 ]] && sleep 2 && xinit &> /dev/null &
 
 timestop l
 title -l '=' "$bar MPD upgraded successfully."
