f014b3e2d8b497158861d375014ce7554c7ed254
rern <rernrern@gmail.com>
2017-10-25 07:09:05 +0700

    Update install.sh

--- mpd/install.sh
+++ mpd/install.sh
@@ -37,15 +37,6 @@ systemctl stop mpd
 
 cp /etc/mpd.conf{.backup,}
 
-echo -e "$bar Modify files ..."
-#sed -i -e '/^Protect/ s/^/#/
-#' -e '/^Restrict/ s/^/#/
-#' /usr/lib/systemd/system/mpd.service
-
-#sed -i -e '/^Protect/ s/^/#/
-#' -e '/^Restrict/ s/^/#/
-#' /usr/lib/systemd/user/mpd.service
-
 # fix permission (default - mpd run by user 'mpd')
 chmod 777 /var/log/runeaudio/mpd.log
 
