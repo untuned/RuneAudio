8d7227c9cae38807a35a0e79b65eada94bcc06e9
rern <rernrern@gmail.com>
2017-10-28 18:50:36 +0700

    Update install.sh

--- sorting/install.sh
+++ sorting/install.sh
@@ -18,6 +18,7 @@ getuninstall
 echo -e "$bar Get files ..."
 wgetnc https://github.com/rern/RuneAudio/raw/master/sorting/sorting.js -P /srv/http/assets/js
 # modify files
+echo -e "$bar Modify files ..."
 footer=/srv/http/app/templates/footer.php
 echo $footer
 if ! grep -q 'sorting.js' /srv/http/app/templates/footer.php; then
