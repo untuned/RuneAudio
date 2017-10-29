a10d7d90f20ded8d55a96adf8ba8885f36d3ad19
rern <rernrern@gmail.com>
2017-10-29 09:06:38 +0700

    Update install.sh

--- breadcrumb/install.sh
+++ breadcrumb/install.sh
@@ -14,6 +14,11 @@ wgetnc https://github.com/rern/RuneAudio/raw/master/breadcrumb/breadcrumb.js -P 
 chown http:http /srv/http/assets/js/breadcrumb.js
 
 echo -e "$bar Modify files ..."
+file=/srv/http/app/templates/header.php
+echo $file
+sed -i $'/runeui.css/ a\
+    <link rel="stylesheet" href="<?=$this->asset(\'/css/breadcrumb.css\')?>">
+' $file
 
 file=/srv/http/app/templates/footer.php
 echo $file
@@ -21,16 +26,17 @@ echo '<script src="<?=$this->asset('"'"'/js/breadcrumb.js'"'"')?>"></script>' >>
 
 file=/srv/http/app/templates/playback.php
 echo $file
-sed -i -e '/id="db-level-up"/ {
+sed -i -e '/id="db-currentpath"/ {N;N; s/^/<!--brea/; s/$/brea-->/}
+' -e '/id="db-level-up"/ {
 s/^/<!--brea/
 s/$/brea-->/
 i\
             <div id="db-currentpath" class="hide">\
                 <i id="db-home" class="fa fa-folder-open"></i> <span>Home</span>\
                 <i id="db-up" class="fa fa-arrow-left"></i>\
+				<i id="db-webradio-add" class="fa fa-plus hide"></i>\
             </div>
 }
-' -e '/id="db-currentpath"/ {N;N; s/^/<!--brea/; s/$/brea-->/}
 ' $file
 
 installfinish $@
