6656feac72ac514682545160653f600d04aebfcf
rern <rernrern@gmail.com>
2017-10-29 21:54:13 +0700

    Update and rename music_library/uninstall_libr.sh to library_enhancement/uninstall_libr.sh

--- library_enhancement/uninstall_libr.sh
+++ library_enhancement/uninstall_libr.sh
@@ -6,9 +6,13 @@ alias=libr
 
 uninstallstart $@
 
+file=/srv/http/app/templates/header.php
+echo $file
+sed -i -e '/libraryenha.css/ d' $file
+
 file=/srv/http/app/templates/footer.php
 echo $file
-sed -i -e '/musiclibrary.js/ d' $file
+sed -i -e '/libraryenha.js/ d' $file
 
 file=/srv/http/app/templates/playback.php
 echo $file
@@ -17,6 +21,6 @@ sed -i -e '/id="db-currentpath" class="hide"/, /<\/div>/ d
 ' -e 's/<!--libr\|libr-->//g
 ' $file
 
-rm /srv/http/assets/js/musiclibrary.js
+rm /srv/http/assets/{css/libraryenha.css,js/libraryenha.js}
 
 uninstallfinish $@
