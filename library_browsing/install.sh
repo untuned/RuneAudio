a817b2e81c2f88f4455408b0b3c298eab8fa4d37
rern <rernrern@gmail.com>
2017-10-29 20:04:26 +0700

    Update install.sh

--- library_browsing/install.sh
+++ library_browsing/install.sh
@@ -6,9 +6,9 @@ alias=sort
 
 . /srv/http/addonstitle.sh
 
-if [[ -e /usr/local/bin/uninstall_webr.sh ]]; then
-	uninstall_webr.sh
-	redis-cli hdel addons webr
+if [[ -e /usr/local/bin/uninstall_libr.sh ]]; then
+	uninstall_libr.sh
+	redis-cli hdel addons libr
 fi
 
 installstart $@
@@ -16,13 +16,13 @@ installstart $@
 getuninstall
 
 echo -e "$bar Get files ..."
-wgetnc https://github.com/rern/RuneAudio/raw/master/sorting/sorting.js -P /srv/http/assets/js
+wgetnc https://github.com/rern/RuneAudio/raw/master/library_browsing/musiclibrary.js -P /srv/http/assets/js
 # modify files
 echo -e "$bar Modify files ..."
 footer=/srv/http/app/templates/footer.php
 echo $footer
-if ! grep -q 'sorting.js' /srv/http/app/templates/footer.php; then
-	echo '<script src="<?=$this->asset('"'"'/js/sorting.js'"'"')?>"></script>' >> $footer
+if ! grep -q 'musiclibrary.js' /srv/http/app/templates/footer.php; then
+	echo '<script src="<?=$this->asset('"'"'/js/musiclibrary.js'"'"')?>"></script>' >> $footer
 fi
 
 installfinish $@
