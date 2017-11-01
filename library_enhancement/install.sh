cf968905cba905231fa4a1615b5762e99787338b
rern <rernrern@gmail.com>
2017-11-01 07:54:23 +0700

    Update install.sh

--- library_enhancement/install.sh
+++ library_enhancement/install.sh
@@ -53,39 +53,46 @@ i\
 }
 ' -e '/id="home-blocks"/ i\
 			<ul id="db-index" class="hide">\
-				<li>#</li>
-				<li>A</li>
-				<li class="half">B</li>
-				<li>C</li>
-				<li class="half">D</li>
-				<li>E</li>
-				<li class="half">F</li>
-				<li>G</li>
-				<li class="half">H</li>
-				<li>I</li>
-				<li class="half">J</li>
-				<li>K</li>
-				<li class="half">L</li>
-				<li>M</li>
-				<li class="half">N</li>
-				<li>O</li>
-				<li class="half">P</li>
-				<li>Q</li>
-				<li class="half">R</li>
-				<li>S</li>
-				<li class="half">T</li>
-				<li>U</li>
-				<li class="half">V</li>
-				<li>W</li>
-				<li class="half">X</li>
-				<li>Y</li>
-				<li class="half">Z</li>
-				<li>&nbsp</li>
-				<li>&nbsp</li>
-				<li>&nbsp</li>
-				<li>&nbsp</li>
-				<li>&nbsp</li>
+				<li>#</li>\
+				<li>A</li>\
+				<li class="half">B</li>\
+				<li>C</li>\
+				<li class="half">D</li>\
+				<li>E</li>\
+				<li class="half">F</li>\
+				<li>G</li>\
+				<li class="half">H</li>\
+				<li>I</li>\
+				<li class="half">J</li>\
+				<li>K</li>\
+				<li class="half">L</li>\
+				<li>M</li>\
+				<li class="half">N</li>\
+				<li>O</li>\
+				<li class="half">P</li>\
+				<li>Q</li>\
+				<li class="half">R</li>\
+				<li>S</li>\
+				<li class="half">T</li>\
+				<li>U</li>\
+				<li class="half">V</li>\
+				<li>W</li>\
+				<li class="half">X</li>\
+				<li>Y</li>\
+				<li class="half">Z</li>\
+				<li>&nbsp</li>\
+				<li>&nbsp</li>\
+				<li>&nbsp</li>\
+				<li>&nbsp</li>\
+				<li>&nbsp</li>\
 			</ul>
+' -e '/id="pl-manage"/,/</div>/ {s/^/<!--libr/; s/$/libr-->/}
+' -e '/id="pl-count"/ i\
+            <div id="pl-manage">\
+                <button id="pl-manage-list" class="btn btn-default" type="button" title="Manage playlists"><i class="fa fa-file-text-o fa-lg"></i></button>\
+                <button id="pl-manage-save" class="btn btn-default" type="button" title="Save current queue as playlist" data-toggle="modal" data-target="#modal-pl-save"><i class="fa fa-save fa-lg"></i></button>\
+                <button id="pl-manage-clear" class="btn btn-default" type="button" title="Clear the playing queue" data-toggle="modal" data-target="#modal-pl-clear"><i class="fa fa-trash-o fa-lg"></i></button>\
+            </div>
 ' $file
 
 installfinish $@
