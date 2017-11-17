26f5595f51024be476cd568ae7dc9d64e55bb6a9
rern <rernrern@gmail.com>
2017-11-17 19:02:59 +0700

    Update install.sh

--- lyrics/install.sh
+++ lyrics/install.sh
@@ -21,9 +21,28 @@ wgetnc https://github.com/rern/RuneAudio/raw/master/lyrics/lyrics.js -P /srv/htt
 chown http:http $file
 
 echo -e "$bar Modify files ..."
+file=/srv/http/app/templates/header.php
+echo $file
+sed -i -e $'/runeui.css/ a\
+    <link rel="stylesheet" href="<?=$this->asset(\'/css/lyrics.css\')?>">
+' -e '/menu-top/ i\
+<div id="lyricsfade" class="hide"></div>
+' $file
+# no RuneUI GPIO
+if ! grep -q 'pnotify.css' $file; then
+	wgetnc https://github.com/rern/RuneAudio/raw/master/lyrics/pnotify.css -P /srv/http/assets/css
+	sed -i $'/runeui.css/ a\    <link rel="stylesheet" href="<?=$this->asset(\'/css/pnotify.css\')?>">' $file
+	chown http:http $file
+fi
+
 file=/srv/http/app/templates/footer.php
 echo $file
 echo '<script src="<?=$this->asset('"'"'/js/lyrics.js'"'"')?>"></script>' >> $file
+# no RuneUI GPIO
+if ! grep -q 'pnotify3.custom.min.js' $file; then
+	wgetnc https://github.com/rern/RuneAudio/raw/master/lyrics/pnotify3.custom.min.js -P /srv/http/assets/js
+	echo '<script src="<?=$this->asset('"'"'/js/vendor/pnotify3.custom.min.js'"'"')?>"></script>' >> $file
+fi
 
 installfinish $@
 
