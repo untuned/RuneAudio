0d61a7c6381bcd8da33446868e3fa8ee7c108528
rern <rernrern@gmail.com>
2017-11-17 19:50:48 +0700

    Update uninstall_lyri.sh

--- lyrics/uninstall_lyri.sh
+++ lyrics/uninstall_lyri.sh
@@ -12,6 +12,14 @@ echo $file
 sed -i '/lyrics.js/ d' $file
 
 echo -e "$bar Remove files ..."
-rm -v /srv/http/assets/js/lyrics.js
+rm -v /srv/http/assets/{js/lyrics.js,css/lyrics.css}
+
+# no RuneUI GPIO
+if [[ ! -e /usr/local/bin/uninstall_gpio.sh ]]; then
+	rm $path/css/pnotify.css
+	rm $path/js/vendor/pnotify3.custom.min.js
+	sed -i '/pnotify.css/ d' $header
+	sed -i '/pnotify3.custom.min.js/ d' $footer
+fi
 
 uninstallfinish $1
