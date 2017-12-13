481121711ed136f620d2e8e710d47407cede3266
rern <rernrern@gmail.com>
2017-12-13 22:44:11 +0700

    Update install.sh

--- nginx/install.sh
+++ nginx/install.sh
@@ -12,8 +12,6 @@ getuninstall
 
 # backup
 mv /etc/nginx/nginx.conf{,.backup}
-bacman nginx-rune
-mv nginx-rune* /var/cache/pacman/pkg
 
 gitpath=https://github.com/rern/RuneAudio/raw/$branch/nginx
 file=nginx-1.13.7-2-armv7h.pkg.tar.xz
