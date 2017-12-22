bf7fb9a370badf0dfe051ab1493cea1dc5abe8ed
rern <rernrern@gmail.com>
2017-12-22 20:26:52 +0700

    Create uninstall_chro.sh

---
+++ chromium/uninstall_chro.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+alias=aria
+
+. /srv/http/addonstitle.sh
+
+uninstallstart $@
+
+# uninstall package #######################################
+pacman -Rs --noconfirm chromium
+
+# restore file
+echo -e "$bar Restore files ..."
+file=/root/.xinitrc
+echo $file
+sed -i -e '/export BROWSER=chromium/ d
+' -e '/^#midori/ s/^#//
+' -e '/^chromium/ d
+' $file
+
+echo -e "$bar Start default local browser..."
+killall Xorg
+xinit &> /dev/null &
+
+uninstallfinish $@
