27c46c4c16667fc7e0069979faa8f95f57233d97
rern <rernrern@gmail.com>
2017-12-27 21:35:19 +0700

    Update install.sh

--- chromium/install.sh
+++ chromium/install.sh
@@ -30,12 +30,21 @@ echo '
 disable_overscan=1
 ' >> $file
 # replace midori with chromium
+if [[ $1 != u ]]; then
+	zoom=$1;
+	zoom=$( echo $zoom | awk '{if ($1 < 0.5) print 0.5; else print $1}' )
+	zoom=$( echo $zoom | awk '{if ($1 > 2) print 2; else print $1}' )
+else
+	zoom=$( redis-cli get enhazoom &> /dev/null )
+	redis-cli del enhazoom &> /dev/null
+fi
+
 file=/root/.xinitrc
 echo $file
 sed -i '/^midori/ {
 s/^/#/
 a\
-chromium --no-sandbox --app=http://localhost --start-fullscreen --force-device-scale-factor='$1'
+chromium --no-sandbox --app=http://localhost --start-fullscreen --force-device-scale-factor='$zoom'
 }
 ' $file
 
