1e815fcb474b79412f928b13a1157841fd2e7b16
rern <rernrern@gmail.com>
2018-01-05 18:08:27 +0700

    Update setupsystem.sh

--- _settings/setupsystem.sh
+++ _settings/setupsystem.sh
@@ -7,7 +7,7 @@
 
 rm $0
 
-[[ ! -e /srv/http/addonstitle.sh ]] && wget -q https://github.com/rern/RuneAudio_Addons/raw/master/srv/http/addonstitle.sh -P /srv/http
+[[ ! -e /srv/http/addonstitle.sh ]] && wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/master/srv/http/addonstitle.sh -P /srv/http
 . /srv/http/addonstitle.sh
 
 mmc() {
