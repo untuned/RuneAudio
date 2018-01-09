c1afe0bf24f4d273ee8695c21084310b0c90f78e
rern <rernrern@gmail.com>
2018-01-09 10:40:33 +0700

    Update cmd.sh

--- _settings/cmd.sh
+++ _settings/cmd.sh
@@ -66,56 +66,6 @@ setup() {
 	fi
 }
 
-resetrune() {
-	. runereset n
-	if [[ $success != 1 ]]; then
-		echo -e "\e[37m\e[41m ! \e[0m RuneAudio reset failed."
-		return
-	fi
-	# preload command shortcuts
-	partroot=$( mount | grep 'on / ' | cut -d' ' -f1 )
-	[[ $partroot == /dev/mmcblk0p9 ]] && partreset=11 || partreset=9
-	mmc $partreset
-	wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/_settings/cmd.sh -P /tmp/p$partreset/etc/profile.d
-	
-	[[ $ansre != 1 ]] && return
-	[[ $partreset == 9 ]] && bootx 8 || bootx 10
-}
-resetosmc() {
-	. osmcreset n
-	if [[ $success != 1 ]]; then
-		echo -e "\e[37m\e[41m ! \e[0m OSMC reset failed."
-		return
-	fi
-	# preload initial setup
-	wget -qN --show-progress https://github.com/rern/OSMC/raw/master/_settings/presetup.sh
-	. presetup.sh
-	# preload command shortcuts
-	mmc 7
-	wget -qN --show-progress https://github.com/rern/OSMC/raw/master/_settings/cmd.sh -P /tmp/p7/etc/profile.d
-	
-	[[ $ansre == 1 ]] && bootosmc
-}
-
-hardreset() {
-	partroot=$( mount | grep 'on / ' | cut -d' ' -f1 )
-	[[ $partroot == /dev/mmcblk0p9 ]] && runeversion=RuneAudio || runeversion=Rune04b
-	echo -e "\n\e[30m\e[43m ? \e[0m Reset to virgin OS:"
-	echo -e '  \e[36m0\e[m Cancel'
-	echo -e "  \e[36m1\e[m $runeversion"
-	echo -e '  \e[36m2\e[m OSMC'
-	echo -e '  \e[36m3\e[m NOOBS: OSMC + Rune'
-	echo
-	echo -e '\e[36m0\e[m / 1 / 2 ? '
-	read -n 1 ans
-	echo
-	case $ans in
-		1) resetrune;;
-		2) resetosmc;;
-		3) noobsreset;;
-		*) ;;
-	esac
-}
 boot() {
 	mmc 5
 	part=$( sed -n '/name/,/mmcblk/ p' /tmp/p5/installed_os.json | sed '/part/ d; s/\s//g; s/"//g; s/,//; s/name://; s/\/dev\/mmcblk0p//' )
