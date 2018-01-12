ac5decd6eeca38d74bff7210f4ebdc33f4a7c0e2
rern <rernrern@gmail.com>
2018-01-12 22:02:00 +0700

    Add files via upload

--- _settings/cmd.sh
+++ _settings/cmd.sh
@@ -2,6 +2,8 @@
 
 alias ls='ls -a --color --group-directories-first'
 export LS_COLORS='tw=01;34:ow=01;34:ex=00;32:or=31'
+info='\e[30m\e[46m i \e[0m'
+yesno='\e[30m\e[43m ? \e[0m'
 
 tcolor() { 
 	echo -e "\e[38;5;10m$1\e[0m"
@@ -36,35 +38,73 @@ sfpm() {
 	systemctl reload php-fpm
 }
 
+mkdir -p /tmp/p5
+mount /dev/mmcblk0p5 /tmp/p5 2> /dev/null
+part=$( sed -n '/name/,/mmcblk/ p' /tmp/p5/installed_os.json | sed '/part/ d; s/\s//g; s/"//g; s/,//; s/name://; s/\/dev\/mmcblk0p//' )
+partarray=( $( echo $part ) )
+ilength=${#partarray[*]}
+bootlist="
+$yesno Reboot to OS:
+  \e[36m0\e[m Cancel
+"
+namearray=('0')
+mountarray=(x)
+for (( i=0; i < ilength; i++ )); do
+	ivalue=${partarray[i]}
+	if (( $(( i % 2 )) == 0 )); then
+		bootlist+="  \e[36m$(( i / 2 + 1 ))\e[m $ivalue\n"
+		namearray+=(${ivalue}-boot ${ivalue}-root)
+	else
+		j=${partarray[i]}
+		mountarray+=($j $(( j + 1 )))
+	fi
+done
+
+ilength=${#mountarray[*]}
+mountlist="
+$yesno Mount SD partition:
+  \e[36m0\e[m Cancel
+"
+for (( i=1; i < ilength; i++ )); do
+	mountlist+="  \e[36m$i\e[0m ${namearray[i]}\n"
+done
+
 mmc() {
-	[[ $2 ]] && mntdir=/tmp/$2 || mntdir=/tmp/p$1
-	if ! mount | grep "$mntdir "; then
+	if (( $# > 0 )); then
+		p=$1
+		if (( $p > 1 && $p < 5 )); then
+			echo -e "$info \e[36m/dev/mmcblk0p$p\e[0m not available."
+			return
+		fi
+	else
+		echo -e "$mountlist"
+		echo
+		echo -e "\e[36m0\e[m / partition ? "
+		read -n 1 ans
+		echo
+		[[ -z $ans || $ans == 0 ]] && return
+		p=${mountarray[$ans]}
+	fi
+	
+	mountline=$( mount | grep "mmcblk0p$p " )
+	name=${namearray[$ans]}
+	if [[ $mountline ]]; then
+		mountpoint=$( echo $mountline | cut -d' ' -f3 )
+		echo -e "$info \e[36m$name\e[0m already mounted at \e[36m$mountpoint\e[0m\n"
+	else
+		mntdir=/tmp/p$p
 		mkdir -p $mntdir
-		mount /dev/mmcblk0p$1 $mntdir
+		mount /dev/mmcblk0p$p $mntdir
+		echo -e "$info \e[36m$name\e[0m mounted at \e[36m$mntdir\e[0m\n"
 	fi
 }
 
 boot() {
-	mmc 5
-	part=$( sed -n '/name/,/mmcblk/ p' /tmp/p5/installed_os.json | sed '/part/ d; s/\s//g; s/"//g; s/,//; s/name://; s/\/dev\/mmcblk0p//' )
-	partarray=( $( echo $part ) )
-
-	ilength=${#partarray[*]}
-	bootarray=(0)
-	
-	echo -e "\n\e[30m\e[43m ? \e[0m Reboot to OS:"
-	echo -e '  \e[36m0\e[m Cancel'
-	for (( i=0; i < ilength; i++ )); do
-		if (( $(( i % 2 )) == 0 )); then
-			echo -e "  \e[36m$(( i / 2 + 1 ))\e[m ${partarray[i]}"
-		else
-			bootarray+=(${partarray[i]})
-		fi
-	done
+	echo -e "$bootlist"
 	echo
-	list=$( seq $(( ${#bootarray[*]} - 1 )) )
-	list=$( echo $list )
-	echo -e "\e[36m0\e[m / ${list// / \/ } ? "
+	bootlistnum=$( seq $(( ${#bootarray[*]} - 1 )) )
+	bootlistnum=$( echo $bootlistnum )
+	echo -e "\e[36m0\e[m / ${bootlistnum// / \/ } ? "
 	read -n 1 ans
 	echo
 	[[ -z $ans || $ans == 0 ]] && return
@@ -102,16 +142,9 @@ else
 	}
 	setup() {
 		if [[ -e /usr/local/bin/uninstall_addo.sh ]]; then
-			echo -e "\e[30m\e[43m ! \e[0m Already setup."
+			echo -e "$info Already setup."
 		else
-			wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/_settings/setup.sh
-			if [[ $? == 5 ]]; then
-				echo -e "\e[38;5;6m\e[48;5;6m . \e[0m Sync time ..."
-				systemctl stop ntpd
-				ntpdate pool.ntp.org
-				systemctl start ntpd
-				wget -qN --show-progress https://github.com/rern/RuneAudio/raw/master/_settings/setup.sh
-			fi
+			wget -qN --no-check-certificate --show-progress https://github.com/rern/RuneAudio/raw/master/_settings/setup.sh
 			chmod +x setup.sh
 			./setup.sh
 		fi
