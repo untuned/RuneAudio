33f25e2bf8573466f06804ef77d21314225819ce
rern <rernrern@gmail.com>
2018-01-16 19:47:15 +0700

    Update install.sh

--- motd/install.sh
+++ motd/install.sh
@@ -40,11 +40,11 @@ echo "
 file=/etc/profile.d/motd.sh
 echo $file
 
-(( $# == 0 )) && colorlogo='\e[38;5;45m' || colorlogo='\e[3'${1}m
+(( $# == 0 )) && color='\e[38;5;45m' || color='\e[3'${1}m
 
 echo '#!/bin/bash
 
-echo -e "'$colorlogo'$( < /etc/motd.logo )\e[0m\n"
+echo -e "'$color'$( < /etc/motd.logo )\e[0m\n"
 ' > $file
 
 echo -e "$bar Modify files ..."
@@ -53,11 +53,10 @@ mv -v /etc/motd{,.original} 2> /dev/null
 
 file=/etc/bash.bashrc
 echo $file
+
 sed -i -e '/PS1=/ s/^/#/
 ' -e '$ a\
-color=242\
-colordir='$colorlogo'\
-PS1=\x27\\[\\e[38;5;\x27$color\x27m\\]\\u@\\h:\\[\\e[38;5;\x27$colordir\x27m\\]\\w \\$\\[\\e[0m\\] \x27
+PS1=\x27\\[\\e[38;5;242m\\]\\u@\\h:\\['$color'\\]\\w \\$\\[\\e[0m\\] \x27
 ' $file
 
 # PS1='\[\e[38;5;'$color'm\]\u@\h:\[\e[0m\]\w \$ '
