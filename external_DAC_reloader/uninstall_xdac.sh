ebd4dbecdc82e1c9e2af2db1bd167a1f4c6b38de
rern <rernrern@gmail.com>
2018-01-27 11:56:17 +0700

    Create uninstall_xdac.sh

---
+++ external_DAC_reloader/uninstall_xdac.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+alias=xdac
+
+. /srv/http/addonstitle.sh
+
+uninstallstart $@
+
+if [[ $1 != u ]]; then
+	redis-cli del aogpio volumegpio acardsgpio mpdconfgpio &> /dev/null
+fi
+
+echo -e "$bar Restore files ..."
+
+rm /srv/http/xdac.php /srv/http/assets/js/xdac.js
+
+echo -e "$bar Restore files ..."
+
+file=/srv/http/app/templates/header.php
+echo $file
+sed -i '/id="xdac"/ d' $file
+
+file=/srv/http/app/templates/mpd.php
+echo $file
+sed -i -e '/id="savexdac"/ d
+' -e '/<script>/,/<\/script>/ d' $file
+
+uninstallfinish $@
