247f1817633d3c5686519006b90630f4b0895ea4
rern <rernrern@gmail.com>
2018-02-01 21:25:10 +0700

    Update uninstall_udac.sh

--- USB_DAC_reloader/uninstall_udac.sh
+++ USB_DAC_reloader/uninstall_udac.sh
@@ -12,21 +12,21 @@ fi
 
 echo -e "$bar Remove files ..."
 
-rm /srv/http/xdac.php /srv/http/assets/js/xdac.js
+rm /srv/http/udac.php /srv/http/assets/js/udac.js
 
 echo -e "$bar Restore files ..."
 
 file=/srv/http/app/templates/header.php
 echo $file
-sed -i '/id="xdac"/ d' $file
+sed -i '/id="udac"/ d' $file
 
 file=/srv/http/app/templates/footer.php
 echo $file
-sed -i '/xdac.js/ d' $file
+sed -i '/udac.js/ d' $file
 
 file=/srv/http/app/templates/mpd.php
 echo $file
-sed -i -e '/xdac0/,/xdac1/ d' $file
+sed -i -e '/udac0/,/udac1/ d' $file
 
 chmod 644 /etc/mpd.conf
 
