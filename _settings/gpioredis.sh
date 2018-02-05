d55cc56941b00b6e43f1810219e3dddd5b5a480d
rern <rernrern@gmail.com>
2018-02-05 19:45:56 +0700

    Add files via upload

---
+++ _settings/gpioredis.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+redis-cli set aogpio 'xCORE USB Audio 2.0'
+redis-cli set volumegpio 0
+
+defaultIFS=$IFS
+IFS=$'\n'
+
+acards=( $( cat gpioacards.txt ) )
+ilength=${#acards[@]}
+for (( i = 0; i < $ilength; i+=2 )); do
+    redis-cli hset acardsgpio "${acards[i]}" "${acards[i+1]}" 
+done
+
+mpdconf=( $( cat gpiompdconf.txt ) )
+ilength=${#mpdconf[@]}
+for (( i = 0; i < $ilength; i+=2 )); do
+    redis-cli hset mpdconfgpio "${mpdconf[i]}" "${mpdconf[i+1]}" 
+done
+
+IFS=$defaultIFS
+
