c3b0d3cbd416ae537a1d448d254ee162a5ae48d1
rern <rernrern@gmail.com>
2018-02-07 11:52:08 +0700

    Update and rename gpioredis.sh to gpio-redis.sh

--- _settings/gpio-redis.sh
+++ _settings/gpio-redis.sh
@@ -6,14 +6,14 @@ redis-cli set volumegpio 0
 defaultIFS=$IFS
 IFS=$'\n'
 
-acards=( $( cat gpioredisacards.txt ) )
+acards=( $( cat gpio-redis-acards.txt ) )
 ilength=${#acards[@]}
 for (( i = 0; i < $ilength; i+=2 )); do
     redis-cli hset acardsgpio "${acards[i]}" "${acards[i+1]}"
 	redis-cli hset acards "${acards[i]}" "${acards[i+1]}"
 done
 
-mpdconf=( $( cat gpioredismpdconf.txt ) )
+mpdconf=( $( cat gpio-redis-mpdconf.txt ) )
 ilength=${#mpdconf[@]}
 for (( i = 0; i < $ilength; i+=2 )); do
     redis-cli hset mpdconfgpio "${mpdconf[i]}" "${mpdconf[i+1]}"
