83597e39d84b523664569883c5c662ee922eca5b
rern <rernrern@gmail.com>
2018-02-07 09:43:23 +0700

    Update gpioredis.sh

--- _settings/gpioredis.sh
+++ _settings/gpioredis.sh
@@ -9,14 +9,14 @@ IFS=$'\n'
 acards=( $( cat gpioacards.txt ) )
 ilength=${#acards[@]}
 for (( i = 0; i < $ilength; i+=2 )); do
-    redis-cli hset acardsgpio "${acards[i]}" "${acards[i+1]}" 
+    redis-cli hset acardsgpio "${acards[i]}" "${acards[i+1]}"
+	redis-cli hset acards "${acards[i]}" "${acards[i+1]}"
 done
 
 mpdconf=( $( cat gpiompdconf.txt ) )
 ilength=${#mpdconf[@]}
 for (( i = 0; i < $ilength; i+=2 )); do
-    redis-cli hset mpdconfgpio "${mpdconf[i]}" "${mpdconf[i+1]}" 
+    redis-cli hset mpdconfgpio "${mpdconf[i]}" "${mpdconf[i+1]}"
 done
 
 IFS=$defaultIFS
-
