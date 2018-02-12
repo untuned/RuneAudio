c446a9bdbb8061368b25d86c6b1cbda1e0d88f3d
rern <rernrern@gmail.com>
2018-02-12 18:16:44 +0700

    Update install.sh

--- USB_DAC_reloader/install.sh
+++ USB_DAC_reloader/install.sh
@@ -15,56 +15,25 @@ installstart $@
 
 getuninstall
 
-echo -e "$bar Add files ..."
-
-file=/srv/http/udac.php
-echo $file
-file=/srv/http/udac.php
-cat > $file <<'EOF'
-<?php
-$redis = new Redis(); 
-$redis->pconnect( '127.0.0.1' );
-
-include( '/srv/http/app/libs/runeaudio.php' );
-
-wrk_audioOutput($redis, 'refresh');
-
-// fix hw:0,N - missing N after wrk_audioOutput($redis, 'refresh')
-$acards = $redis->hGetAll( 'acards' );
-foreach ( $acards as $key => $value ) {
-	preg_match( '/"id":"."/', $value, $match );
-	$id = preg_replace( '/"id":"(.)"/', '${1}', $match[ 0 ] );
-	$subdevice = $id ? $id - 1 : 0;
-	$value1 = preg_replace( '/(hw:.,)/', '${1}'.$subdevice, $value );
-	$redis->hSet( 'acards', $key, $value1 );
-}
-EOF
-
-file=/srv/http/assets/js/udac.js
-echo $file
-cat > $file <<'EOF'
-$( "#acardsreload" ).click( function() {
-	$.get( "/udac.php", function() {
-		location.reload();
-	} );
-} );
-EOF
-
 echo -e "$bar Modify files ..."
 
-file=/srv/http/app/templates/footer.php
+file=/etc/udev/rules.d/rune_usb-audio.rules
 echo $file
-sed -i '$ a\
-<script src="<?=$this->asset('"'"'/js/udac.js'"'"')?>"></script>
+sed -i '/SUBSYSTEM=="sound"/ s/^/#/
+' -e '$ \a
+ACTION=="add", KERNEL=="card*", SUBSYSTEM=="sound", RUN+="/var/www/command/refresh_ao on"\
+ACTION=="remove", KERNEL=="card*", SUBSYSTEM=="sound", RUN+="/var/www/command/refresh_ao"
 ' $file
+file=/srv/http/command/refresh_ao
 
-file=/srv/http/app/templates/mpd.php
 echo $file
-# with non-unique id fix
-sed -i -e '/This switches output/ i\
-						<a class="btn btn-primary btn-lg" id="udac"><i class="fa fa-refresh fa-lg" style="margin-top: -10px;"></i></a>
-' -e 's/id="log-level"\( name="conf\[user\]"\)/id="user"\1/
-' -e 's/id="log-level"\( name="conf\[state_file\]"\)/id="state"\1/
+sed -i $'/close Redis/ i\
+if ( $argc > 1 ) {\
+	// "exec" gets only last line which is new power-on card\
+	$ao = exec( \'/usr/bin/aplay -lv | grep card | cut -d"]" -f1 | cut -d"[" -f2\' );\
+	$redis->set( "ao", $ao );\
+	wrk_mpdconf( $redis, "switchao", $ao );\
+}
 ' $file
 
 installfinish $@
