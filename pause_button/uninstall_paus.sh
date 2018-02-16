c0f80edf229cdd8837cb29c5d181f5ce6fc35f3c
rern <rernrern@gmail.com>
2018-02-16 21:05:16 +0700

    Update uninstall_paus.sh

--- pause_button/uninstall_paus.sh
+++ pause_button/uninstall_paus.sh
@@ -14,7 +14,7 @@ sed -i '/id="pause"/ d' $file
 
 file=/srv/http/assets/js/runeui.js
 echo $file
-sed -i -e 's/\/\* paus\|paus \*\///
+sed -i -e 's/\/\*paus\|paus\*\///
 ' -e '/paus0/,/paus1/ d
 ' $file
 
