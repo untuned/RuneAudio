bf6054aeb5299b2408da8b60a89e6e7d3aad0255
rern <rernrern@gmail.com>
2018-03-10 09:35:46 +0700

    Create install.sh

---
+++ volume/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# change version number in RuneAudio_Addons/srv/http/addonslist.php
+
+alias=volu
+
+. /srv/http/addonstitle.sh
+
+installstart $@
+
+getuninstall
+
+echo -e "$bar Add new files ..."
+
+file=/srv/http/assets/js/volume.js
+echo $file
+wgetnc https://github.com/rern/RuneAudio/raw/master/volume/volume.js -O $file
+
+file=/srv/http/assets/js/vendor/propagating.js
+echo $file
+[[ ! -e $file ]] && wgetnc https://github.com/rern/RuneAudio/raw/master/volume/propagating.js -O $file
+
+file=/srv/http/assets/js/vendor/hammer.min.js
+echo $file
+[[ ! -e $file ]] && wgetnc https://github.com/rern/RuneAudio/raw/master/volume/propagating.js -O $file
+
+echo -e "$bar Modify files ..."
+
+file=/srv/http/app/templates/footer.php
+echo $file
+sed -i $'$ a\<script src="<?=$this->asset(\'/js/volume.js\')?>"></script>' $file
+! grep -q 'hammer.min.js' $file && sed -i $'$ a\<script src="<?=$this->asset(\'/js/vendor/hammer.min.js\')?>"></script>' $file
+! grep -q 'propagating.js' $file && sed -i $'$ a\<script src="<?=$this->asset(\'/js/vendor/propagating.js\')?>"></script>' $file
+
+installfinish $@
+
+clearcache
