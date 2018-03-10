797c87bbd8ce7bcc0843ec19ea8e5adda204e67c
rern <rernrern@gmail.com>
2018-03-10 09:45:13 +0700

    Create uninstall_volu.sh

---
+++ volume/uninstall_volu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+alias=volu
+
+. /srv/http/addonstitle.sh
+
+uninstallstart $@
+
+echo -e "$bar Remove files ..."
+
+rm /srv/http/assets/js/volume.js
+if [[ ! -e /usr/local/bin/uninstall_enha.sh ]]; then
+  rm /srv/http/assets/js/vendor/{propagating.js,hammer.min.js}
+  sed '/hammer.min.js\|propagating.js/ d' /srv/http/app/templates/footer.php
+fi
+
+echo -e "$bar Restore files ..."
+file=/srv/http/app/templates/footer.php
+echo $file
+sed -i '/volume.js/ d' $file
+
+uninstallfinish $@
