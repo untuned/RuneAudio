29f0d8624ac428a58ddfce1388ba5e5704024ab1
rern <rernrern@gmail.com>
2018-04-08 08:27:27 +0700

    Add files via upload

--- DSD_bitrate/dsdbitrate.sh
+++ DSD_bitrate/dsdbitrate.sh
@@ -12,19 +12,19 @@
 #| dsd512 | 22579200 | 01588800 |  8800 0158  |  5801 0088  |
 #|----------------------------------------------------------|
 
-ext=$( echo $1 | sed 's/^.*\.//' | tr '[:lower:]' '[:upper:]' )
+ext=$( echo "$1" | sed 's/^.*\.//' | tr '[:lower:]' '[:upper:]' )
 
 IFS0=$IFS
 IFS=$( echo -en "\n\b" )
 if [[ $ext == DSF ]]; then
-	hexword=$( hexdump -x -s56 -n4 $file )
+	hexword=$( hexdump -x -s56 -n4 $1 )
 	# string=0000040 <5758> <5960> 000003c
 	IFS=$IFS0
 	hex=( $( echo $hexword | cut -d' ' -f2,3 ) )
 	# bitrate byte order: #59#60#57#58
 	bitrate=$( echo $(( 16#${hex[1]}${hex[0]} )) )
 else # DFF
-	hexword=$( hexdump -x -s60 -n4 $file )
+	hexword=$( hexdump -x -s60 -n4 $1 )
 	# string=000003c <6162> <6364> 0000040
 	IFS=$IFS0
 	hex=( $( echo $hexword | cut -d' ' -f2,3 | tr -d ' ' | sed 's/.\{2\}/& /g' ) )
