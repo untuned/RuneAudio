b09f9798719c4bfdfcbbe237b4b04e975d12961c
rern <rernrern@gmail.com>
2018-06-20 21:17:42 +0700

    Update notify_duration.sh

--- notify_duration/notify_duration.sh
+++ notify_duration/notify_duration.sh
@@ -5,4 +5,6 @@
 sed -i "s/[0-9]000\( : notify.delay\)/$(( $1 * 1000 ))\1/" /srv/http/assets/js/runeui.js
 sed -i "s/[0-9]e3\(:notify.delay\)/${1}e3\1/" /srv/http/assets/js/runeui.min.js
 
+redis-cli set notifysec $1
+
 title -l '=' "$bar Notification duration changed to $1 seconds"
