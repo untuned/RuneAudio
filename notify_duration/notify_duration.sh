a7b45aba8866872471266016825a4da154097e9b
rern <rernrern@gmail.com>
2018-06-20 16:40:05 +0700

    Update notify_duration.sh

--- notify_duration/notify_duration.sh
+++ notify_duration/notify_duration.sh
@@ -2,7 +2,7 @@
 
 . /srv/http/addonstitle.sh
 
-sed 's/8000\( : notify.delay\)/$(( 1 * 1000 ))\1/' /srv/http/assets/js/runeui.js
-sed 's/8e3\(:notify.delay\)/${1}e3\1/' /srv/http/assets/js/runeui.min.js
+sed -i 's/8000\( : notify.delay\)/$(( 1 * 1000 ))\1/' /srv/http/assets/js/runeui.js
+sed -i 's/8e3\(:notify.delay\)/${1}e3\1/' /srv/http/assets/js/runeui.min.js
 
 title -l '=' "$bar Notification duration changed to $1 seconds"
