da923ae61d0bab0f31c8883a2300ad7e326bffbd
rern <rernrern@gmail.com>
2018-07-05 09:11:26 +0700

    Update install.sh

--- ui_reset/install.sh
+++ ui_reset/install.sh
@@ -51,6 +51,52 @@ rm -f /srv/http/assets/js/vendor/bootstrap-select-1.12.1.min.js
 rm -f /srv/http/{lyrics.php,lyricssave.php,simple_html_dom.php}
 rm -f /srv/http/assets/{js/lyrics.js,css/lyrics.css}
 
+# udac
+sed -i -e '/SUBSYSTEM=="sound"/ s/^#//
+' -e '/^ACTION/ d
+' /etc/udev/rules.d/rune_usb-audio.rules
+udevadm control --reload-rules && udevadm trigger
+sed -i -e '/ui_notify/ s|^//||
+' -e '/udac0/,/udac1/ d' /srv/http/command/refresh_ao
+
+# back
+sed -i -e '\|/run/backup_|,+1 s|^//||
+' -e '\|/srv/http/tmp|,/^ \+;/ d
+' /srv/http/app/libs/runeaudio.php
+
+sed -i -e 's/id="restore"/method="post"/
+' -e 's/type="file" name="filebackup"/type="file"/
+' -e '/id="btn-backup-upload"/ s/id="btn-backup-upload"/& name="syscmd" value="restore"/; s/disabled>Restore/type="submit" disabled>Upload/
+' /srv/http/app/templates/settings.php
+
+rm -f /etc/sudoers.d/http-backup
+rm -f /srv/http/restore.*
+rm -f /srv/http/assets/js/restore.js
+rm -fr /srv/http/tmp
+
+# spla
+sed -i -e 's/ logo.nologo//' /boot/cmdline.txt
+sed '/disable_splash=1/ d' /boot/confix.txt
+
+systemctl disable ply-image
+systemctl enable getty@tty1.service
+
+rm -f /etc/systemd/system/ply-image.service
+rm -f /usr/local/bin/ply-image
+rm -fr /usr/share/ply-image
+
+# font
+rm -fr /srv/http/assets/fonts/lato
+mv -f /srv/http/assets/fonts/lato{.backup,}
+
+#motd
+mv -f /etc/motd{.original,} 2> /dev/null
+rm -f /etc/motd.logo /etc/profile.d/motd.sh
+
+sed -i -e '/^PS1=/ d
+' -e '/^#PS1=\|#export PS1=/ s/^#//
+' /etc/bash.bashrc
+
 # pass
 if [[ $version != 0.4b ]]; then
 	rm -f /srv/http/log*
