e8b9c3e3455bd768867946777d41884647ffd230
rern <rernrern@gmail.com>
2018-07-05 16:28:54 +0700

    Update install.sh

--- ui_reset/install.sh
+++ ui_reset/install.sh
@@ -10,45 +10,16 @@ alias=uire
 title -l '=' "$bar Reset RuneUI ..."
 timestart
 
-# addo
-rm -f /srv/http/assets/css/addons*
-rm -f /srv/http/assets/fonts/addons.*,Inconsolata.*
-rm -f /srv/http/assets/js/addons*
-rm -f /srv/http/assets/js/vendor/{hammer.min.js,propagating.js}
-rm -f /srv/http/addons*
-rm -fr /srv/http/assets/addons
 crontab -l | { cat | sed '/addonsupdate.sh/ d'; } | crontab -
 
 # enha
-rm -f /srv/http/app/templates/enhanceplayback.php
-rm -f /srv/http/enhance*
-rm -f /srv/http/assets/css/{enhance.css,midori.css,roundslider.min.css}
-rm -f /srv/http/assets/fonts/enhance*
-rm -f /srv/http/assets/img/{controls*,runelogo.svg,turntable*}
-rm -f /srv/http/assets/js/enhance.js
-rm -f /srv/http/assets/js/vendor/{jquery-ui.min.js,modernizr-custom.js,roundslider.min.js}
 rm -f /usr/share/bootsplash/{start,reboot,shutdown}-runeaudio.png
 
 # gpio
 rm -f /root/gpio*.py
-rm -f /srv/http/gpio*.php
-rm -f /srv/http/assets/css/gpio*
-rm -f /srv/http/assets/img/RPi3_GPIO.svg
-rm -f /srv/http/assets/js/gpio*
-rm -f /srv/http/assets/js/vendor/bootstrap-select-1.12.1.min.js
-
-# lyri
-rm -f /srv/http/{lyrics.php,lyricssave.php,simple_html_dom.php}
-rm -f /srv/http/assets/{js/lyrics.js,css/lyrics.css}
 
 # back
 rm -f /etc/sudoers.d/http-backup
-rm -f /srv/http/restore.*
-rm -f /srv/http/assets/js/restore.js
-rm -fr /srv/http/tmp
-
-# font
-rm -fr /srv/http/assets/fonts/lato*
 
 #motd
 rm -f /etc/motd.logo /etc/profile.d/motd.sh
@@ -58,8 +29,6 @@ if [[ $version == 20170229 ]]; then
 	file=ui_reset.tar.xz
 else
 	file=ui_reset03.tar.xz
-	# pass
-	rm -f /srv/http/log*
 	# spla
 	systemctl disable ply-image
 	systemctl enable getty@tty1.service
