56be1f4a113d60c276e46a242e7fe9301950867a
rern <rernrern@gmail.com>
2018-07-05 08:50:43 +0700

    Update install.sh

--- ui_reset/install.sh
+++ ui_reset/install.sh
@@ -11,6 +11,23 @@ version=$( redis-cli get buildversion )
 title -l '=' "$bar Reset RuneUI ..."
 timestart
 
+rm -f /srv/http/app/templates/enhanceplayback.php
+rm -f /srv/http/enhance*
+rm -f /srv/http/assets/css/{enhance.css,midori.css,roundslider.min.css}
+rm -f /srv/http/assets/fonts/enhance*
+rm -f /srv/http/assets/img/{controls*,runelogo.svg,turntable*}
+rm -f /srv/http/assets/js/enhance.js
+rm -f /srv/http/assets/js/vendor/{jquery-ui.min.js,modernizr-custom.js,roundslider.min.js}
+rm -f /usr/share/bootsplash/{start,reboot,shutdown}-runeaudio.png
+
+mv /srv/http/assets/fonts/fontawesome-webfont.woff{.backup,} 2>/dev/null
+mv /srv/http/assets/fonts/fontawesome-webfont.ttf{.backup,} 2>/dev/null
+mv /srv/http/assets/js/runeui.min.js{.backup,} 2>/dev/null
+mv /srv/http/app/coverart_ctl.php{.backup,} 2>/dev/null
+mv /usr/share/bootsplash/start-runeaudio.png{.backup,} 2>/dev/null
+mv /usr/share/bootsplash/reboot-runeaudio.png{.backup,} 2>/dev/null
+mv /usr/share/bootsplash/shutdown-runeaudio.png{.backup,} 2>/dev/null
+
 wgetnc https://github.com/rern/RuneAudio/raw/$branch/ui_rest/ui-reset.tar.xz
 bsdtar -xvf ui-reset.tar.xz -C /srv/http
 rm ui_reset.tar.xz
