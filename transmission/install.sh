0695e7e08d54c244c810934beb41bdf4fddebdd4
rern <rernrern@gmail.com>
2018-07-15 12:28:55 +0700

    Update install.sh

--- transmission/install.sh
+++ transmission/install.sh
@@ -13,19 +13,17 @@ installstart $@
 getuninstall
 
 gitpath=https://github.com/rern/RuneAudio/raw/$branch/transmission
-wgetnc $gitpath/libcrypto.so.1.1 -P /usr/lib
-wgetnc $gitpath/libssl.so.1.1 -P /usr/lib
+wgetnc $gitpath/_repo/transmission/transmission-cli-2.92-6-armv7h.pkg.tar.xz
 
-pacman -Sy --noconfirm libevent transmission-cli
+pacman -U --noconfirm transmission-cli-2.92-6-armv7h.pkg.tar.xz
 
-lnfile=$( find /lib/libevent* -type f | grep '.*/libevent-.*' )
-ln -sf $lnfile /lib/libevent-2.0.so.5
+rm transmission-cli-2.92-6-armv7h.pkg.tar.xz
 
 # remove conf for non-exist user 'transmission'
 rm /usr/lib/tmpfiles.d/transmission.conf
 
 if mount | grep -q '/dev/sda1'; then
-	mnt=$( mount | grep '/dev/sda1' | cut -d' ' -f3 )
+	mnt=$( mount | grep '/dev/sda1' | awk '{ print $3 }' )
 	mkdir -p $mnt/transmission
 	path=$mnt/transmission
 else
@@ -35,13 +33,12 @@ fi
 mkdir -p $path/{incomplete,watch}
 
 # custom systemd unit
-ln -sf /lib/systemd/system/tran{smission,}.service
-systemctl stop tran
-systemctl disable tran
+ln -s /lib/systemd/system/trans{mission,}.service
+systemctl stop trans
+systemctl disable trans
 
-# custom user and env - TRANSMISSION_HOME must be /<path>/transmission-daemon
 dir=/etc/systemd/system/transmission.service.d
-mkdir -p $dir
+mkdir $dir
 echo "[Service]
 User=root
 Environment=TRANSMISSION_HOME=$path
@@ -49,11 +46,11 @@ Environment=TRANSMISSION_WEB_HOME=$path/web
 " > $dir/override.conf
 systemctl daemon-reload
 
-file=$path/settings.json
-rm -f $file
 # create settings.json
-systemctl start tran
-systemctl stop tran
+file=$path/settings.json
+[[ -e $file ]] && rm $file
+systemctl start trans
+systemctl stop trans
 
 if [[ $1 != u ]]; then
 	sed -i -e 's|"download-dir": ".*"|"download-dir": "'"$path"'"|
@@ -61,54 +58,51 @@ if [[ $1 != u ]]; then
 	' -e 's|"incomplete-dir-enabled": false|"incomplete-dir-enabled": true|
 	' -e 's|"rpc-whitelist-enabled": true|"rpc-whitelist-enabled": false|
 	' -e '/[^{},\{, \}]$/ s/$/, /
-	' -e '/^}$/ i\
-    "watch-dir": "'"$path"'/watch", \
-    "watch-dir-enabled": true
+	' -e '/}/ i\
+	    "watch-dir": "'"$path"'/watch", \
+	    "watch-dir-enabled": true
 	' $file
+
 	# set password
 	if [[ -n $1 && $1 != 0 ]]; then
 		sed -i -e 's|"rpc-authentication-required": false|"rpc-authentication-required": true|
 		' -e 's|"rpc-password": ".*"|"rpc-password": "'"$1"'"|
 		' -e 's|"rpc-username": ".*"|"rpc-username": "root"|
 		' $file
-	else
-		sed -i 's|"rpc-authentication-required": true|"rpc-authentication-required": false|
-		' $file
 	fi
 else
-	mv -f /tmp/settings.json $file
+	mv /tmp/settings.json $path
 fi
 
 # web ui alternative
 if [[ $2 == 1 ]] || [[ $( redis-cli get tranwebui ) ]]; then
 	echo -e "$bar Get WebUI alternative ..."
-	file=src.tar.gz
-	wgetnc https://github.com/ronggang/twc-release/raw/master/$file
+	wgetnc https://github.com/ronggang/transmission-web-control/raw/master/release/transmission-control-full.tar.gz
 	rm -rf $path/web
 	mv /usr/share/transmission/web $path
 	mv $path/web/index{,.original}.html
-	bsdtar -xf $file -C $path/web
-	rm $file
+	bsdtar -xf transmission-control-full.tar.gz -C $path
+	rm transmission-control-full.tar.gz
 	chown -R root:root $path/web
 	redis-cli del tranwebui &> /dev/null
 fi
 
 systemctl daemon-reload
 if [[ $3 == 1 ]] || [[ $( redis-cli get transtartup ) ]]; then
-	systemctl enable tran
+	systemctl enable trans
 	redis-cli del transtartup &> /dev/null
 fi
 
 echo -e "$bar Start Transmission ..."
-if ! systemctl start tran &> /dev/null; then
+if ! systemctl start trans &> /dev/null; then
 	title -l = "$warn Transmission install failed."
 	exit
 fi
 
 installfinish $@
 
-echo "Run: systemctl < start / stop > tran"
-echo "Startup: systemctl < enable / disable > tran"
+echo "Run: systemctl < start / stop > trans"
+echo "Startup: systemctl < enable / disable > trans"
 echo
 echo "Download directory: $path"
 echo "WebUI: < RuneAudio_IP >:9091"
