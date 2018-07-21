e13ea4089823d36d3ef01ac3924560fc5cac5c3d
rern <rernrern@gmail.com>
2018-07-21 16:08:35 +0700

    Update shairport-sync.js

--- shairport-sync/shairport-sync.js
+++ shairport-sync/shairport-sync.js
@@ -12,44 +12,57 @@ pushstreamAirplay.onmessage = function( status ) { // on receive broadcast
 	var time = atob( status[ '70726772' ] );   // prgr
 	var coverart = status[ '50494354' ];       // PICT - base64 string
 	var time = time.split( '/' );
-	var total = converthms( ( time[ 2 ] - time[ 1 ] ) / 44100 );
-	var elapsed = converthms( ( time[ 0 ] - time[ 1 ] ) / 44100 );
+	var total = Math.round( ( time[ 2 ] - time[ 1 ] ) / 44100 );
+	var elapsed = Math.round( ( time[ 0 ] - time[ 1 ] ) / 44100 );
 
 //function displayairplay() {
-	$( '.playback-controls' ).css( 'visibility', 'hidden' );
+	$( '#menu-top, #menu-bottom' ).toggleClass( 'hide', !display.bar );
+	if ( display.bar ) $( '.playback-controls' ).css( 'visibility', 'hidden' );
+	
 	$( '#divartist, #divsong, #divalbum' ).removeClass( 'scroll-left' );
 	$( '#playlist-position span, #format-bitrate, #total' ).html( '&nbsp;' );
 	$( '#currentartist' ).html( artist );
 	$( '#currentsong' ).html( song );
 	$( '#currentalbum' ).html( album );
+	scrolltext();
+	
 	var imgcode = coverart.charAt[ 0 ];
 	var imgtype = { '/': 'jpg', 'i': 'png', 'R': 'gif' };
 	$( '#cover-art' ).css( {
 		  'background-image': 'url("data:image/'+ imgtype[ imgcode ] +';base64,'+ coverart +'")'
 		, 'border-radius': 0
 	} );
-	scrolltext();
-	$( '#menu-top, #menu-bottom' ).toggleClass( 'hide', !display.bar );
-	$( '#playback-row' ).removeClass( 'hide' );
+	
 	$( '#imode i, #coverartoverlay, #volume-knob, #play-group, #share-group, #vol-group' ).addClass( 'hide' );
-	$( '#playsource-mpd' ).addClass( 'inactive' );
-	$( '#playsource-airplay' ).removeClass( 'inactive' );
 	$( '#elapsed, #total' ).html( '&nbsp;' );
 	$( '#time-knob' ).toggleClass( 'hide', !display.time );
 	if ( display.time ) {
 		$( '#time-knob, #play-group, #coverart, #share-group' ).css( 'width', '45%' );
 		clearInterval( GUI.currentKnob );
 		clearInterval( GUI.countdown );
-		$( '#time' ).roundSlider( 'setValue', 0 );
-		$( '#elapsed' ).css( 'color', '#e0e7ee' );
-		$( '#total' ).text( '' );
-		$( '#iplayer' ).addClass( 'fa-airplay' ).removeClass( 'hide' );
-		var elapsed = 0;
+		var position = Math.round( elapsed / time * 1000 );
+		$( '#elapsed' ).text( converthms( elapsed ) ).css( 'color', '#e0e7ee' );
+		$( '#total' ).text( converthms( time ) );
+		GUI.currentKnob = setInterval( function() {
+			position = position + 1;
+			if ( position === 1000 ) {
+				clearInterval( GUI.currentKnob );
+				clearInterval( GUI.countdown );
+				$( '#elapsed' ).text( '' );
+			}
+			$( '#time' ).roundSlider( 'setValue', position );
+		}, time );
+		
 		GUI.countdown = setInterval( function() {
 			elapsed++
 			mmss = converthms( elapsed );
 			$( '#elapsed' ).text( mmss );
 		}, 1000 );
+		$( '#iplayer' ).addClass( 'fa-airplay' ).removeClass( 'hide' );
+		
+		$( '#playback-row' ).removeClass( 'hide' );
+		$( '#playsource-mpd' ).addClass( 'inactive' );
+		$( '#playsource-airplay' ).removeClass( 'inactive' );
 	} else {
 		$( '#coverart, #share-group' ).css( 'width', '60%' );
 	}
