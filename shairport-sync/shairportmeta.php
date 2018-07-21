9f0584ab8ce515a2c652600b094407bcc1749bad
rern <rernrern@gmail.com>
2018-07-21 08:13:36 +0700

    Update shairportmeta.php

--- shairport-sync/shairportmeta.php
+++ shairport-sync/shairportmeta.php
@@ -1,15 +1,24 @@
 <?php
+$replace = array(
+	  '#<item><type>.*</type><code>.*</code><length>0</length>.*\\n#' => ''
+	, '#<item><type>.*</type><code>(.*)</code><length>.*</length>.*#' => "$1"
+	, '#<data encoding="base64">\\n#' => ''
+	, '#(.*)</data></item>#' => "$1"
+	, '#</item>#' => ''
+);
 $airplay_handle = fopen('/tmp/shairport-sync-metadata', 'r' );
-stream_set_blocking( $airplay_handle, false );
-while ( 1 ) {
+//stream_set_blocking( $airplay_handle, false );
+while ( !feof( $airplay_handle ) ) {
 	$line = fgets( $airplay_handle );
-//	if ( preg_match( '/61736172|6d696e6d|6173616c|70726772/', $line ) ) continue;
-	$line = preg_replace( '/<item>|<type>.*<\/type><code>|<length>.*<\/length>|<data encoding="base64">|<\/data>|<\/item>|\\n/', '', $line );
-	$code = preg_replace( '/(.*)<\/code>.*/', "\ncode: $1\n", $line );
-	$data = preg_replace( '/(.*)<\/code>.*/', 'data: ', $line );
-	echo $code;
-	echo $data;
+	$std = preg_replace(
+		array_keys( $replace ),
+		array_values( $replace ),
+		$line
+	);
+	echo $std;
 }
+fclose( $airplay_handle );
 
-// code: hex string
-// data: base64 string
+// output:
+// hex string code
+// base64 string data
