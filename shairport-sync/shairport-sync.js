ebf1cba055d8cf1f41f3f1333472a7cc8f3c3259
rern <rernrern@gmail.com>
2018-07-22 16:35:57 +0700

    Update shairport-sync.js

--- shairport-sync/shairport-sync.js
+++ shairport-sync/shairport-sync.js
@@ -26,8 +26,8 @@ pushstreamAirplay.onmessage = function( status ) { // on receive broadcast
 	$( '#currentalbum' ).html( album );
 	scrolltext();
 	
-//	var imgcode = coverart.charAt[ 0 ];
-//	var imgtype = { '/': 'jpg', 'i': 'png', 'R': 'gif' };
+//	var imgcodetype = { '/': 'jpeg', 'i': 'png', 'R': 'gif' };
+//  var imgtype = imgcodetype[ coverart.charAt[ 0 ] ];
 	$( '#cover-art' ).css( {
 		  'background-image': 'url("data:image/jpeg;base64,'+ coverart +'")'
 		, 'border-radius': 0
@@ -68,43 +68,5 @@ pushstreamAirplay.onmessage = function( status ) { // on receive broadcast
 		clearInterval( GUI.currentKnob );
 		clearInterval( GUI.countdown );
 	}
-//}
 };
 pushstreamAirplay.connect();
-
-function xmlToJson(xml) { // from https://davidwalsh.name/convert-xml-json
-	// Create the return object
-	var obj = {};
-
-	if (xml.nodeType == 1) { // element
-		// do attributes
-		if (xml.attributes.length > 0) {
-		obj["@attributes"] = {};
-			for (var j = 0; j < xml.attributes.length; j++) {
-				var attribute = xml.attributes.item(j);
-				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-			}
-		}
-	} else if (xml.nodeType == 3) { // text
-		obj = xml.nodeValue;
-	}
-
-	// do children
-	if (xml.hasChildNodes()) {
-		for(var i = 0; i < xml.childNodes.length; i++) {
-			var item = xml.childNodes.item(i);
-			var nodeName = item.nodeName;
-			if (typeof(obj[nodeName]) == "undefined") {
-				obj[nodeName] = xmlToJson(item);
-			} else {
-				if (typeof(obj[nodeName].push) == "undefined") {
-					var old = obj[nodeName];
-					obj[nodeName] = [];
-					obj[nodeName].push(old);
-				}
-				obj[nodeName].push(xmlToJson(item));
-			}
-		}
-	}
-	return obj;
-};
