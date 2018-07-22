548808f8c816aa9a95f173c41fe2a49553042c97
rern <rernrern@gmail.com>
2018-07-22 18:25:56 +0700

    Update uninstall_shai.sh

--- shairport-sync/uninstall_shai.sh
+++ shairport-sync/uninstall_shai.sh
@@ -7,7 +7,7 @@ alias=shai
 
 uninstallstart $@
 
-rm /srv/http/shairport.php /srv/http/assets/js/shairport-sync.js
+rm /srv/http/shairport-sync.php /srv/http/assets/js/shairport-sync.js
 
 restorefile /srv/http/app/templates/footer.php /srv/http/command/rune_PL_wrk
 
