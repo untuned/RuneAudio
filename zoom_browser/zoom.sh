9eb9b7572e0940b526361ab840a71d34900686dd
rern <rernrern@gmail.com>
2018-07-22 09:18:21 +0700

    Update zoom.sh

--- zoom_browser/zoom.sh
+++ zoom_browser/zoom.sh
@@ -2,9 +2,14 @@
 
 . /srv/http/addonstitle.sh
 
-sed -i "s/^\(zoom-level=\).*/\1$1" /root/.config/midori/config
-sed -i "s/^\(force-device-scale-factor=\).*/\1$1" /root/.xinitrc
+if ! pacman -Q chromium &> /dev/null; then
+    sed -i "s/^\(zoom-level=\).*/\1$1" /root/.config/midori/config
+else
+    sed -i "s/\(force-device-scale-factor=\).*/\1$1" /root/.xinitrc
+fi
 
 redis-cli set zoomlevel $1 &> /dev/null
+
+clearcache
 
 title -l '=' "$bar Zoom level of local browser changed to $1"
