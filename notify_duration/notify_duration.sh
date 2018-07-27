d1b17129a0705e360d09d16f4c4a3ad40245bc67
rern <rernrern@gmail.com>
2018-07-27 09:59:42 +0700

    Update notify_duration.sh

--- notify_duration/notify_duration.sh
+++ notify_duration/notify_duration.sh
@@ -2,7 +2,7 @@
 
 . /srv/http/addonstitle.sh
 
-title -l '=' "$bar Chanhe notification duration ..."
+title -l '=' "$bar Change notification duration ..."
 
 sed -i "s/[0-9]000\( : notify.delay\)/$(( $1 * 1000 ))\1/" /srv/http/assets/js/runeui.js
 sed -i "s/[0-9]e3\(:notify.delay\)/${1}e3\1/" /srv/http/assets/js/runeui.min.js
