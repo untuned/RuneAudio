74ea4a08108c8b24c7a5afd7ba198c6911e30db5
rern <rernrern@gmail.com>
2018-07-31 07:28:41 +0700

    Update set.sh

--- set_notify/set.sh
+++ set_notify/set.sh
@@ -9,6 +9,6 @@ title -l '=' "$bar Change notification duration ..."
 sed -i "s/[0-9]000\( : notify.delay\)/$(( $1 * 1000 ))\1/" /srv/http/assets/js/runeui.js
 sed -i "s/[0-9]e3\(:notify.delay\)/${1}e3\1/" /srv/http/assets/js/runeui.min.js
 
-redis-cli set setnotify $1 &> /dev/null
+redis-cli hset settings notify $1 &> /dev/null
 
 title -nt "$info Notification duration changed to $1 seconds"
