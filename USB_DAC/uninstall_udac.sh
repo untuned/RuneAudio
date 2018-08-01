229f09b3b6b05976c1ad796b0e730ed49704c047
rern <rernrern@gmail.com>
2018-08-01 16:36:15 +0700

    Update uninstall_udac.sh

--- USB_DAC/uninstall_udac.sh
+++ USB_DAC/uninstall_udac.sh
@@ -3,24 +3,14 @@
 alias=udac
 
 . /srv/http/addonstitle.sh
+. /srv/http/addonsedit.sh
 
 uninstallstart $@
 
 echo -e "$bar Restore files ..."
 
-file=/etc/udev/rules.d/rune_usb-audio.rules
-echo $file
-sed -i -e '/SUBSYSTEM=="sound"/ s/^#//
-' -e '/^ACTION/ d
-' $file
+restorefile /etc/udev/rules.d/rune_usb-audio.rules file=/srv/http/command/refresh_ao
 
 udevadm control --reload-rules && udevadm trigger
-
-file=/srv/http/command/refresh_ao
-echo $file
-sed -i -e '/ui_notify/ s|^//||
-' -e '/udac0/,/udac1/ d' $file
-
-redis-cli del udaclist &> /dev/null
 
 uninstallfinish $@
