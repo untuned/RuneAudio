11f29e9371aff91f99f635f5a4c70cbe1ac4e00a
rern <rernrern@gmail.com>
2018-08-01 16:32:49 +0700

    Update install.sh

--- USB_DAC/install.sh
+++ USB_DAC/install.sh
@@ -5,6 +5,7 @@
 alias=udac
 
 . /srv/http/addonstitle.sh
+. /srv/http/addonsedit.sh
 
 installstart $@
 
@@ -15,33 +16,35 @@ echo -e "$bar Modify files ..."
 file=/etc/udev/rules.d/rune_usb-audio.rules
 echo $file
 # split add-remove to suppress notify twice
-sed -i -e '/SUBSYSTEM=="sound"/ s/^/#/
-' -e '$ a\
-ACTION=="add", KERNEL=="card*", SUBSYSTEM=="sound", RUN+="/var/www/command/refresh_ao on"\
+commentS 'SUBSYSTEM=="sound"'
+
+string=$( cat <<'EOF'
+ACTION=="add", KERNEL=="card*", SUBSYSTEM=="sound", RUN+="/var/www/command/refresh_ao on"
 ACTION=="remove", KERNEL=="card*", SUBSYSTEM=="sound", RUN+="/var/www/command/refresh_ao off"
-' $file
+EOF
+)
+appendS 'SUBSYSTEM=="sound"'
 
 udevadm control --reload-rules && udevadm trigger
 
 file=/srv/http/command/refresh_ao
 echo $file
 # $1 = ao@name
-sed -i -e '/ui_notify/ s|^|//|
-' -e $'/close Redis/ i\
-// udac0\
-if ( $argc > 1 ) {\
-	if ( $argv[ 1 ] == "on" ) {\
-		// "exec" gets only last line which is new power-on card\
-		$ao = exec( \'/usr/bin/aplay -lv | grep card | cut -d"]" -f1 | cut -d"[" -f2\' );\
-		$name = $ao;\
-	} else {\
-		$ao = "'"${1%@*}"'";\
-		$name = "'"${1#*@}"'";\
-	}\
-	ui_notify( "Audio Output", "Switch to ".$name );\
-	wrk_mpdconf( $redis, "switchao", $ao );\
-}\
-// udac1
-' $file
+string=$( cat <<'EOF'
+if ( $argc > 1 ) {
+	if ( $argv[ 1 ] == "on" ) {
+		// "exec" gets only last line which is new power-on card
+		$ao = exec( '/usr/bin/aplay -lv | grep card | cut -d"]" -f1 | cut -d"[" -f2' );
+		$name = $ao;
+	} else {
+		$ao = "'"${1%@*}"'";
+		$name = "'"${1#*@}"'";
+	}
+	ui_notify( "Audio Output", "Switch to ".$name );
+	wrk_mpdconf( $redis, "switchao", $ao );
+}
+EOF
+)
+insert 'close Redis'
 
 installfinish $@
