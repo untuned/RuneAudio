680d62e63cc1b3a03696331caad1d0814d72faa1
rern <rernrern@gmail.com>
2018-08-02 12:17:05 +0700

    Update webradiodb.sh

--- webradio/webradiodb.sh
+++ webradio/webradiodb.sh
@@ -2,7 +2,6 @@
 
 rm $0
 
-[[ ! -e /srv/http/addonstitle.sh ]] && wget -q https://github.com/rern/RuneAudio_Addons/raw/master/srv/http/addonstitle.sh -P /srv/http
 . /srv/http/addonstitle.sh
 
 # if sub directories
@@ -13,9 +12,9 @@ if ls -d /mnt/MPD/Webradio/*/ &> /dev/null; then
 	rm -rf /mnt/MPD/Webradio/{*,.[^.]}/
 fi
 
-if ! ls /mnt/MPD/Webradio/*.pls &> /dev/null; then
+if ! ls /mnt/MPD/Webradio/*.{pls,m3u} &> /dev/null; then
 	title -l '=' "$info No webradio files found."
-	title -nt 'Copy *.pls to /mnt/MPD/Webradio/ then run again.'
+	title -nt 'Copy *.pls or *.m3u to /mnt/MPD/Webradio/ then run again.'
 	exit
 fi
 
@@ -25,26 +24,67 @@ title -l '=' "$bar Webradio Import ..."
 redis-cli del webradios &> /dev/null
 
 # add data from files
-for file in /mnt/MPD/Webradio/*.pls; do
-	count=$( grep -c '^File' "$file" )
-	for (( i=1; i <= $count; i++ )); do
-		url=$( grep "^File$i" "$file" | cut -d '=' -f2 )
-		name=$( grep "^Title$i" "$file" | cut -d '=' -f2 )
-		redis-cli hset webradios "$name" "$url" &> /dev/null
-		printf "%-30s : $url\n" "$name"
-		
-		if (( $count > 1 )); then
-			string=$( cat <<EOF
+makefile() {
+	string=$( cat <<EOF
 [playlist]
 NumberOfEntries=1
-File1=$url
-Title1=$name
+Title1=$1
+File1=$2
 EOF
 )
-			echo "$string" > /mnt/MPD/Webradio/"$name".pls
-		fi
-	done
-	(( $count > 1 )) && rm "$file"
+	echo "$string" > /mnt/MPD/Webradio/"$1".pls
+}
+incrementname() {
+	if (( ${#names[@]} > 0 )); then
+		for n in "${names[@]}"; do
+			if [[ $n == $1 ]]; then
+				[[ -n $( echo $n | grep ' #[0-9]\+$' ) ]] && num=${n##*\#} || num=1
+				(( num++ ))
+				name=$1' #'$num
+			fi
+		done
+	fi
+	names+=( $name )
+}
+
+for file in /mnt/MPD/Webradio/*; do
+	if [[ ${file##*.} == pls ]]; then
+		# count to work with multiple items
+		count=$( grep -c '^File' "$file" )
+		for (( i=1; i <= $count; i++ )); do
+			name=$( grep "^Title$i" "$file" | cut -d '=' -f2 )
+			# no name
+			[[ -z $name ]] && name="noName"
+			# increment same name
+			incrementname "$name"
+			url=$( grep "^File$i" "$file" | cut -d '=' -f2 )
+			printf "%-30s : $url\n" "$name"
+			redis-cli hset webradios "$name" "$url" &> /dev/null
+			# delete if has only 1 item
+			(( $count == 1 )) && rm "$file"
+			makefile "$name" "$url"
+		done
+		# delete after gone through all items
+		(( $count > 1 )) && rm "$file"
+	else
+		# *.m3u
+		cat $file | while read line; do
+			[[ ${line:0:4} != http ]] && continue
+			
+			linenohttp=${line:7}
+			if [[ $linenohttp =~ '/' ]]; then
+				filename=${linenohttp##*/}
+				name=${filename%.*}
+			else
+				name="noName"
+			fi
+			incrementname "$name"
+			printf "%-30s : $line\n" "$name"
+			redis-cli hset webradios "$name" "$url" &> /dev/null
+			makefile "$name" "$line"
+		done
+		rm "$file"
+	fi
 done
 
 # refresh list
