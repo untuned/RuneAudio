081c00d5ef796f278d59169e060cb3d97e8d4b70
rern <rernrern@gmail.com>
2018-08-10 14:00:16 +0700

    Update uninstall_paus.sh

--- pause_button/uninstall_paus.sh
+++ pause_button/uninstall_paus.sh
@@ -3,20 +3,16 @@
 alias=paus
 
 . /srv/http/addonstitle.sh
+. /srv/http/addonsedit.sh
 
 uninstallstart $@
 
+rm -v /srv/http/assets/css/pausebutton.css
+
 echo -e "$bar Restore files ..."
 
-file=/srv/http/app/templates/header.php
-echo $file
-sed -i -e '/pausebutton.css/ d
-' -e '/id="pause"/ d' $file
-
-file=/srv/http/assets/js/runeui.js
-echo $file
-sed -i -e '/\/\*paus\|paus\*\// d
-' -e '/paus0/,/paus1/ d
-' $file
+restorefile /srv/http/app/templates/header.php /srv/http/assets/js/runeui.js
 
 uninstallfinish $@
+
+clearcache
