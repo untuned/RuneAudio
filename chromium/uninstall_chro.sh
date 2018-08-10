2ab5ac7f659d8a657bcdf4a63dcf095271671a42
rern <rernrern@gmail.com>
2018-08-10 10:35:05 +0700

    Update uninstall_chro.sh

--- chromium/uninstall_chro.sh
+++ chromium/uninstall_chro.sh
@@ -15,21 +15,12 @@ file=/boot/cmdline.txt
 echo $file
 sed -i '1 s/$/ ipv6.disable=1/' $file
 
-file=/boot/config.txt
-echo $file
-sed -i '/disable_overscan=1/ d' $file
-
-file=/root/.xinitrc
-echo $file
-sed -i -e '/^#midori/ s/^#//
-' -e '/^chromium/ d
-' $file
-
-if [[ $1 != u ]]; then
-	echo -e "$bar Start default local browser..."
-	killall Xorg
-	sleep 3
-	xinit &> /dev/null &
-fi
+files="
+/boot/config.txt
+/root/.xinitrc
+"
+restorefile $files
 
 uninstallfinish $@
+
+clearcache
