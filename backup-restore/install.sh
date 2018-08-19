5a87dd48f1a69a85293b948ca60ef54bd3a8ceea
rern <rernrern@gmail.com>
2018-08-19 09:58:18 +0700

    Update install.sh

--- backup-restore/install.sh
+++ backup-restore/install.sh
@@ -5,56 +5,87 @@
 alias=back
 
 . /srv/http/addonstitle.sh
+. /srv/http/addonsedit.sh
 
 installstart $@
 
 getuninstall
 
 echo -e "$bar Modify files ..."
+
 dir=/srv/http/tmp
 echo $dir
 mkdir -p $dir
-
+#----------------------------------------------------------------------------------
 file=/srv/http/app/libs/runeaudio.php
-if ! grep -q 'bsdtar -czpf' $file; then
 echo $file
-sed -i -e '\|/run/backup_|,+1 s|^|//|
-' -e '\|/run/backup_| i\
-        $filepath = "/srv/http/tmp/backup_".date("Y-m-d").".tar.gz";\
-        $cmdstring = "rm -f /srv/http/tmp/backup_* &> /dev/null; ".\
-            "redis-cli save; ".\
-            "bsdtar -czpf $filepath".\
-                " --exclude /etc/netctl/examples ".\
-                "/etc/netctl ".\
-                "/mnt/MPD/Webradio ".\
-                "/var/lib/redis/rune.rdb ".\
-                "/var/lib/mpd ".\
-                "/etc/mpd.conf ".\
-                "/etc/mpdscribble.conf ".\
-                "/etc/spop"\
+
+comment -n +1 '/run/backup_'
+
+string=$( cat <<'EOF'
+        $filepath = "/srv/http/tmp/backup_".date("Y-m-d").".tar.gz";
+        $cmdstring = "rm -f /srv/http/tmp/backup_* &> /dev/null; ".
+            "redis-cli save; ".
+            "bsdtar -czpf $filepath".
+                " --exclude /etc/netctl/examples ".
+                "/etc/netctl ".
+                "/mnt/MPD/Webradio ".
+                "/var/lib/redis/rune.rdb ".
+                "/var/lib/mpd ".
+                "/etc/mpd.conf ".
+                "/etc/mpdscribble.conf ".
+                "/etc/spop"
         ;
-' $file
-fi
-
+EOF
+)
+insert -n +1 '/run/backup_'
+#----------------------------------------------------------------------------------
 file=/srv/http/app/templates/settings.php
-if ! grep -q 'filebackup' $file; then
 echo $file
-sed -i -e '/value="backup"/ {n;n;n;n;n;n; s/method="post"/id="restore"/}
-' -e 's/type="file"/& name="filebackup"/
-' -e'/value="restore"/ s/name="syscmd" value="restore" //; s/type="submit" disabled>Upload/disabled>Restore/
-' $file
-fi
 
+commentH -n -3 'Restore player config'
+
+string=$( cat <<'EOF'
+    <form class="form-horizontal" id="restore">
+EOF
+)
+appendH -n -3 'Restore player config'
+
+commentH 'Browse...'
+
+string=$( cat <<'EOF'
+    <form class="form-horizontal" id="restore">
+EOF
+)
+appendH 'Browse...'
+
+commentH 'id="btn-backup-upload"'
+
+string=$( cat <<'EOF'
+                    <button id="btn-backup-upload" class="btn btn-primary btn-lg" disabled>Restore</button>
+EOF
+)
+appendH 'id="btn-backup-upload"'
+#----------------------------------------------------------------------------------
 file=/srv/http/app/templates/footer.php
-if ! grep -q 'restore.js' $file; then
-	echo $file
-	echo '<script src="<?=$this->asset('\''/js/restore.js'\'')?>"></script>' >> $file
-fi
+echo $file
+
+[[ -e $file.backup ]] && file=$file.backup
+
+string=$( cat <<'EOF'
+<script src="<?=$this->asset('/js/restore.js')?>"></script>
+EOF
+)
+appendH '$'
+#----------------------------------------------------------------------------------
 
 echo -e "$bar Add new files ..."
+
+#----------------------------------------------------------------------------------
 file=/srv/http/assets/js/restore.js
 echo $file
-echo '
+
+string=$( cat <<'EOF'
 $("#restore").submit(function() {
     var formData = new FormData($(this)[0]);
     $.ajax({
@@ -71,11 +102,15 @@ $("#restore").submit(function() {
     });
     return false
 });
-' > $file
-
+EOF
+)
+echo "$string" > $file
+#----------------------------------------------------------------------------------
 file=/srv/http/restore.php
 echo $file
-echo '<?php
+
+string=$( cat <<'EOF'
+<?php
 $file = $_FILES["filebackup"];
 $filename = $file["name"];
 $filetmp = $file["tmp_name"];
@@ -94,11 +129,15 @@ if ($restore == 0) {
 } else {
 	echo "Restore failed !";
 }
-' > $file
-
+EOF
+)
+echo "$string" > $file
+#----------------------------------------------------------------------------------
 file=/srv/http/restore.sh
 echo $file
-echo '#!/bin/bash
+
+string=$( cat <<'EOF'
+#!/bin/bash
 
 systemctl stop mpd redis
 bsdtar -xpf $1 -C /
@@ -108,12 +147,19 @@ hostnamectl set-hostname $( redis-cli get hostname )
 sed -i "s/opcache.enable=./opcache.enable=$( redis-cli get opcache )/" /etc/php/conf.d/opcache.ini
 
 rm $1
-' > $file
-
+EOF
+)
+echo "$string" > $file
+#----------------------------------------------------------------------------------
 file=/etc/sudoers.d/http-backup
 echo $file
-echo 'http ALL=NOPASSWD: ALL' > $file
 
+string=$( cat <<'EOF'
+http ALL=NOPASSWD: ALL
+EOF
+)
+echo "$string" > $file
+#----------------------------------------------------------------------------------
 chmod 755 /srv/http/restore.* /srv/http/tmp
 chown http:http /srv/http/restore.* /srv/http/tmp
 
