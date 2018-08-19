75d082d1e7b1093e28210d1896aa1401215fd915
rern <rernrern@gmail.com>
2018-08-19 09:22:45 +0700

    Update uninstall_back.sh

--- backup-restore/uninstall_back.sh
+++ backup-restore/uninstall_back.sh
@@ -9,14 +9,19 @@ uninstallstart $@
 
 echo -e "$bar Restore files ..."
 files="
+/srv/http/app/libs/runeaudio.php
 /srv/http/app/templates/footer.php
 /srv/http/app/templates/settings.php
 "
 restorefile $files
 
 rm -v /etc/sudoers.d/http-backup
-rm -v /srv/http/backuprestore.*
-rm -v /srv/http/assets/js/backuprestore.js
+rm -v /srv/http/restore.*
+rm -v /srv/http/assets/js/restore.js
 rm -rv /srv/http/tmp
 
 uninstallfinish $@
+
+title -nt "Please wait 5 seconds before continue."
+
+systemctl restart rune_SY_wrk
