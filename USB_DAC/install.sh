1ed90bc69bd95daf06fe77e58f5766c042ab377f
rern <rernrern@gmail.com>
2018-08-25 16:24:25 +0700

    Update install.sh

--- USB_DAC/install.sh
+++ USB_DAC/install.sh
@@ -19,33 +19,39 @@ echo $file
 commentS 'SUBSYSTEM=="sound"'
 
 string=$( cat <<'EOF'
-ACTION=="add", KERNEL=="card*", SUBSYSTEM=="sound", RUN+="/var/www/command/refresh_ao on"
-ACTION=="remove", KERNEL=="card*", SUBSYSTEM=="sound", RUN+="/var/www/command/refresh_ao off"
+ACTION=="add", KERNEL=="card*", SUBSYSTEM=="sound", RUN+="/var/www/command/udac.php on"
+ACTION=="remove", KERNEL=="card*", SUBSYSTEM=="sound", RUN+="/var/www/command/udac.php"
 EOF
 )
 appendS 'SUBSYSTEM=="sound"'
 
 udevadm control --reload-rules && udevadm trigger
 
-file=/srv/http/command/refresh_ao
-echo $file
-
 string=$( cat <<'EOF'
+<?php
+$redis = new Redis();
+$redis->connect('/tmp/redis.sock');
+include('/var/www/app/libs/runeaudio.php');
+
 if ( $argc > 1 ) {
-	if ( $argv[ 1 ] === "on" ) {
-		// "exec" gets only last line which is new power-on card
-		$ao = exec( '/usr/bin/aplay -lv | grep card | cut -d"]" -f1 | cut -d"[" -f2' );
-		$name = $ao;
-	} else {
-		$ao = $redis->get( 'aodefault' );
-		$name = $redis->hGet( 'udaclist', $ao );
-	}
-	ui_notify( 'Audio Output', 'Switch to '.$name );
-	wrk_mpdconf( $redis, 'switchao', $ao );
+	// "exec" gets only last line which is new power-on card
+	$ao = exec( '/usr/bin/aplay -lv | grep card | cut -d"]" -f1 | cut -d"[" -f2' );
+	$name = $ao;
+} else {
+	$ao = $redis->get( 'aodefault' );
+	$name = $redis->hGet( 'udaclist', $ao );
+}
+// broadcast
+if ( file_exists( /srv/http/gpiosettings.php ) {
+	ui_render( 'gpio', $name );
+} else {
+	ui_notify( 'Audio Output Switch', $name );
+}
+wrk_mpdconf( $redis, 'switchao', $ao );
 }
 EOF
 )
-insert 'close Redis'
+echo "string" /srv/http/udac.php
 
 redis-cli set aodefault "$1" &> /dev/null
 
