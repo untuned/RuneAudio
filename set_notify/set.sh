b4a5723315dbd36cd834e994bd9be4ec8a244001
rern <rernrern@gmail.com>
2018-10-28 20:11:23 +0700

    Update set.sh

--- set_notify/set.sh
+++ set_notify/set.sh
@@ -7,7 +7,7 @@ rm $0
 title -l '=' "$bar Change notification duration ..."
 
 if [[ -e /srv/http/enhance.php ]]; then
-	sed -i "s/\(PNotify.prototype.options.delay = \)[0-9]\+$/\1$(( $1 * 1000 ))/" /srv/http/assets/js/enhance.js
+	sed -i "s/\(PNotify.prototype.options.delay = \)[0-9]\+/\1$(( $1 * 1000 ))/" /srv/http/assets/js/enhance.js
 else
 	sed -i "s/[0-9]\+\( : notify.delay\)/$(( $1 * 1000 ))\1/" /srv/http/assets/js/runeui.js
 	sed -i "s/[0-9]\+e3\(:notify.delay\)/${1}e3\1/" /srv/http/assets/js/runeui.min.js
