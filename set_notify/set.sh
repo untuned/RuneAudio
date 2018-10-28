afd2a618fdd6cd7e413a35540f038014fd94cb05
rern <rernrern@gmail.com>
2018-10-28 07:49:26 +0700

    Update set.sh

--- set_notify/set.sh
+++ set_notify/set.sh
@@ -7,13 +7,11 @@ rm $0
 title -l '=' "$bar Change notification duration ..."
 
 if [[ -e /srv/http/enhance.php ]]; then
-	#sed -i "s/\(notify.delay : \)[0-9]\+$/\1$(( $1 * 1000 ))/" /srv/http/assets/js/enhancefunction.js
 	sed -i "s/\(PNotify.prototype.options.delay = \)[0-9]\+$/\1$(( $1 * 1000 ))/" /srv/http/assets/js/enhance.js
+else
+	sed -i "s/[0-9]\+\( : notify.delay\)/$(( $1 * 1000 ))\1/" /srv/http/assets/js/runeui.js
+	sed -i "s/[0-9]\+e3\(:notify.delay\)/${1}e3\1/" /srv/http/assets/js/runeui.min.js
 fi
-
-sed -i "s/[0-9]\+\( : notify.delay\)/$(( $1 * 1000 ))\1/" /srv/http/assets/js/runeui.js
-sed -i "s/[0-9]\+e3\(:notify.delay\)/${1}e3\1/" /srv/http/assets/js/runeui.min.js
-
 redis-cli hset settings notify $1 &> /dev/null
 
 title -nt "$info Notification duration changed to $1 seconds"
